e81b9053784eb71a022ed17a60c515dd713281ee
Jos Verlinde <jos_verlinde@hotmail.com>
2019-04-23 23:12:33 +0200

    add generation of *.pyi types from modules

--- runOnPc/1_downloadFrozen.py
+++ runOnPc/1_downloadFrozen.py
@@ -4,7 +4,12 @@ Collect modules and python stubs from other projects
 import requests
 import os
 
-def download_file(url , folder ="./"):
+try:
+    os.chdir('./runonpc')
+except:
+    pass
+
+def download_file(url, folder = "./"):
     local_filename = folder + url.split('/')[-1]
     # NOTE the stream=True parameter below
     with requests.get(url, stream=True) as r:
@@ -24,19 +29,34 @@ def download_files(repo, frozen_modules, savepath):
         url = repo.format(mod)
         download_file(url, savepath  )
 
+
+def make_stubfiles(modulepath):
+    cmd = "python make_stub_files.py -c ./make_stub_files.cfg -u {}*.py".format(modulepath)
+    os.system(cmd)
+
+
+
 #Loboris frozen modules 
 frozen_modules = ["README.md","ak8963.py","freesans20.py","functools.py","logging.py","microWebSocket.py","microWebSrv.py","microWebTemplate.py","mpu6500.py","mpu9250.py","pye.py","ssd1306.py","tpcalib.py","upip.py",
 "upip_utarfile.py","upysh.py","urequests.py","writer.py"] 
 repo = 'https://raw.githubusercontent.com/loboris/MicroPython_ESP32_psRAM_LoBo/master/MicroPython_BUILD/components/micropython/esp32/modules/{}'
 
-savepath = './stubs/esp32_lobo_frozen/'
-download_files(repo, frozen_modules, savepath )
+modulepath = '../stubs/esp32_lobo_frozen/'
 
-#pyboard custom stub 
+#download 
+download_files(repo, frozen_modules, modulepath )
+#now generate / update stubs 
+make_stubfiles(modulepath)
 
-savepath = './stubs/pyb_common/'
-modules = ['pyb.py']
-url = 'https://raw.githubusercontent.com/dastultz/micropython-pyb/master/lib/{}'
-download_files(url, modules,  savepath )
+
+
+# pyboard custom stub is included and doesn not need to be downloaded.
+modulepath = '../stubs/pyb_common/'
+    ## modules = ['pyb.py']
+    ## url = 'https://raw.githubusercontent.com/dastultz/micropython-pyb/master/lib/{}'
+    ## download_files(url, modules,  savepath )
+#now generate / update stubs 
+make_stubfiles(modulepath)
+
 
 
