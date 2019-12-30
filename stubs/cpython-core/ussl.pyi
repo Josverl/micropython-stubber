151b06b67ffe47fb667562e024060feee94d5d73
Jos Verlinde <jos_verlinde@hotmail.com>
2019-12-30 04:43:02 +0100

    the stubs

---
+++ stubs/cpython-core/ussl.pyi
@@ -0,0 +1,6 @@
+
+from typing import Any, Dict, Optional, Sequence, Tuple, Union
+Node = Any
+def wrap_socket(*args, **kw) -> Any: ...
+    #   0: return uio.UioStream(s.makefile('rwb',buffering=0),is_bin=bool)
+    # ? 0: return uio.UioStream(str.makefile(str, buffering=number), is_bin=bool)
