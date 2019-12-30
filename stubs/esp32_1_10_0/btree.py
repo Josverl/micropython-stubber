e123d1feea046f887f9ab603af75c17bab5b7ae3
Jos Verlinde <jos_verlinde@hotmail.com>
2019-12-30 05:08:13 +0100

    Move stubs to branch:  stub/ALL

--- stubs/esp32_1_10_0/btree.py
+++
@@ -1,6 +0,0 @@
-"Module 'btree' on firmware 'v1.10-247-g0fb15fc3f on 2019-03-29'"
-DESC = 2
-INCL = 1
-def open():
-    pass
-
--- stubs/esp32_1_10_0/ussl.py
+++
@@ -1,4 +0,0 @@
-"Module 'ussl' on firmware 'v1.10-247-g0fb15fc3f on 2019-03-29'"
-def wrap_socket():
-    pass
-
