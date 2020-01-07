a0c26516e472c5c6f2cacd9f80f4a3d654d9cc44
Jos Verlinde <jos_verlinde@hotmail.com>
2020-01-08 00:06:34 +0100

    fix basicgit test issues

--- src/basicgit.py
+++ src/basicgit.py
@@ -1,6 +1,14 @@
-"simple Git module"
+"simple Git module, where needed via powershell "
 import subprocess
 import os
+
+
+def scriptpath(script: str):
+    try:
+        _scriptpath = os.path.dirname(os.path.realpath(__file__))
+    except:
+        _scriptpath = "./src"
+    return os.path.join(_scriptpath, script)
 
 def get_tag(repo: str = None) -> str:
     """
@@ -18,14 +26,15 @@ def get_tag(repo: str = None) -> str:
     try:
         result = subprocess.run(cmd, capture_output=True, check=True)
         if result.stderr != b'':
-            print(result.stderr.decode("utf-8"))        # todo: raise error 
-            return None
-        tag:str = result.stdout.decode("utf-8")
+            print(result.stderr.decode("utf-8"))
+            raise Exception(result.stderr.decode("utf-8"))
+        tag: str = result.stdout.decode("utf-8")
         tag = tag.replace('\r', '').replace('\n', '')
         return tag
     except  subprocess.CalledProcessError as e:
-        print("Error: ", e.returncode, e.stderr)        # todo: raise error 
-        return None
+        print("Error: ", e.returncode, e.stderr)
+        raise Exception(e.stderr)
+
 
 
 def checkout_tag(tag: str, repo: str = None) -> bool:
@@ -40,70 +49,30 @@ def checkout_tag(tag: str, repo: str = None) -> bool:
     elif repo.endswith('.git'):
         repo = os.path.basename(repo)
 
-    cmd = ['git', 'checkout', 'tags/'+ tag]
+    cmd = ["pwsh", scriptpath('git-checkout-tag.ps1'), '-repo', repo,'-tag' , tag]
     try:
-        result = subprocess.run(cmd, capture_output=True, check=True, cwd='../micropython')
-        if result.stderr != b'':
-            print(result.stderr.decode("utf-8"))        # todo: raise error 
-            return False
-        print(result.stdout)
-        return True
+        result = subprocess.run(cmd, capture_output=True, check=True)
+        if result.returncode >= 0:
+            # actually a good result
+            print(result.stderr.decode("utf-8"))
+            return True
+        else: 
+            raise Exception(result.stderr.decode("utf-8"))
+
     except  subprocess.CalledProcessError as e:
-        print("Error: ", e.returncode, e.stderr)        # todo: raise error 
+        print(e)
         return False
 
 
-# # get_gittag.ps1 
-# def get_tag_ps1(repo: str = None) -> str:
-#     """
-#     get the most recent git version tag of a local repo"
-#     repo should be in the form of : path/.git
-#         ../micropython/.git
-#     returns the tag or None
-#     """
-#     if not repo:
-#         repo = './.git'
-#     elif not repo.endswith('.git'):
-#         repo += '/.git'
-
-#     try:
-#         scriptpath = os.path.dirname(os.path.realpath(__file__))
-#     except:
-#         scriptpath = "./src"
-#     scriptpath = os.path.join(scriptpath, "get-gittag.ps1")
-
-#     cmd = ["pwsh", scriptpath, repo]
-#     try:
-#         result = subprocess.run(cmd, capture_output=True, check=True)
-#         if result.stderr != b'':
-#             print(result.stderr.decode("utf-8"))
-#             return None
-#         tag:str = result.stdout.decode("utf-8")
-#         tag = tag.replace('\r', '').replace('\n', '')
-#         return tag
-
-#     except OSError as e:
-#         print(e)
-#         return None
-
-def test():
-    # very simple test 
-    print( get_tag() ) 
-    print( get_tag('.') ) 
-    print( get_tag('../micropython') ) 
-
-
-    x = get_tag('../micropython')    
-    checkout_tag('v1.11', repo= '../micropython')
-    print( get_tag('../micropython') ) 
-    checkout_tag(x, repo= '../micropython')
-    print( get_tag('../micropython') ) 
-
-
-    # the below should fail
-    print( get_tag('../micropyt') ) 
-    print( get_tag('./git') ) 
-
-if __name__ == "__main__":
-    test()
-    
+    # todo: retry withouth powershell
+    # cmd = ['git', 'checkout', 'tags/'+ tag]
+    # try:
+    #     result = subprocess.run(cmd, capture_output=True, check=True, cwd='../micropython')
+    #     if result.stderr != b'':
+    #         print(result.stderr.decode("utf-8"))        # todo: raise error 
+    #         return False
+    #     print(result.stdout)
+    #     return True
+    # except  subprocess.CalledProcessError as e:
+    #     print("Error: ", e.returncode, e.stderr)        # todo: raise error 
+    #     return False
