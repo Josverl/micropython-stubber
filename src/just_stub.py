598969ea735d1eca00661b178825ef1ae9816b16
Jos Verlinde <jos_verlinde@hotmail.com>
2020-01-08 11:56:32 +0100

    add test for just_stub

--- src/just_stub.py
+++ src/just_stub.py
@@ -1,4 +1,7 @@
 "just create typeshed stubs"
 from download_frozen import make_stub_files
-make_stub_files('./stubs', levels=7)
+def just_stub(levels):
+    make_stub_files('./stubs', levels)
 
+if __name__ == "__main__":
+    just_stub(7)
---
+++ src/just_stub_test.py
@@ -0,0 +1,6 @@
+
+#SOT
+import just_stub
+
+def test_just_stub():
+    just_stub.just_stub(2)
