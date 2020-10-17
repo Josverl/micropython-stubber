de78d957a24612e826baa5ec954dd354667a98ab
Jos Verlinde <josverl@microsoft.com>
2020-10-17 23:22:07 +0200

    Autostart if not stubbed yet

--- board/main.py
+++ board/main.py
@@ -1,8 +1,17 @@
 
-# import time
-# print('start in 3...')
-# time.sleep(3)
-# import createstubs
+import uos as os
+import time
+
+
+try: 
+    # only run import if no stubs yet
+    os.listdir('stubs')
+    print("stub folder was found, stubbing is not automatically started")
+except OSError:
+    for i in range(5, 0, -1):
+        print('start stubbing in {}...'.format(i))
+        time.sleep(1)
+    import createstubs
 
 
 
