e700ac12dcdc6d3e761282f7dec4b2506f047281
Jos Verlinde <josverl@microsoft.com>
2020-10-21 22:04:47 +0200

    add utils mod change mod naming create manifest for cpython

--- src/get_all_frozen.py
+++ src/get_all_frozen.py
@@ -6,83 +6,29 @@ Collect modules and python stubs from other projects
 # Copyright (c) 2020 Jos Verlinde
 # MIT license
 # pylint: disable= line-too-long
-import glob
-import os
-import shutil
-import subprocess
 import logging
 
 import basicgit as git
-import freezer_mpy
-import freezer_lobo
 
-log = logging.getLogger(__name__)
+import utils
+from utils import clean_version, stubfolder, flat_version
 
-STUB_FOLDER = './all-stubs'
+import get_cpython
+import get_mpy
+import get_lobo
 
 # todo: add frozen modules for :
 # import freezer_pycopy
 # import freezer_pycom
 
-def make_stub_files(stub_path, levels: int = 1):
-    "generate typeshed files for all scripts in a folder"
-    level = ""
-    # make_sub_files.py only does one folder level at a time
-    # so lets try 7 levels /** ,  /**/** , etc
-    for i in range(levels):
-        cmd = "py ./src/make_stub_files.py -c ./src/make_stub_files.cfg -u {}{}/*.py".format(stub_path, level)
-        log.debug("level {} : {}".format(i+1, cmd))
-        os.system(cmd)
-        level = level + '/**'
+log = logging.getLogger(__name__)
 
-#def pip_download(requirements, path:str):
-def get_cpython(requirements, stub_path=None):
-    "Download MicroPython compatibility modules"
-    if not stub_path:
-        stub_path = './stubs/cpython-core'
-    # use pip to dowload requirements file to build folder in one go
-    #   pip install --no-compile --no-cache-dir --target ./scratch/test --upgrade -r ./src/microcpython.txt
-    build_path = os.path.abspath("./build")
-    os.makedirs(stub_path, exist_ok=True)
-    os.makedirs(build_path, exist_ok=True)
-    try:
-        subprocess.run(["pip", "install", "--target", build_path, "-r", requirements,
-                        "--no-cache-dir", "--no-compile", "--upgrade", "--no-binary=:all:"],
-                        capture_output=False, check=True)
-        # copy *.py files in build folder to stub_path
-        for filename in glob.glob(os.path.join(build_path, "*.py")):
-            log.info("pipped : {}".format(filename))
-            try:
-                shutil.copy2(filename, stub_path)
-            except OSError as err:
-                log.exception(err)
-    except OSError as err:
-        log.error("An error occurred while trying to run pip to dowload the MicroPython compatibility modules from PyPi: {}".format(err))
-    finally:
-        # remove build folder
-        shutil.rmtree(build_path, ignore_errors=True)
-
-def flat_version(version: str):
-    "Turn version from 'v1.2.3' into '1_2_3' to be used in filename"
-    return version.replace('v', '').replace('.', '_')
-
-def clean_version(version:str, build:bool = False):
-    "omit the commit hash from the git tag"
-    # 'v1.13-103-gb137d064e' --> 'v1.13-103'
-    nibbles = version.split('-')
-    if len(nibbles) == 1:
-        return version
-    elif build:
-        return '-'.join(version.split('-')[0:-1])
-    else:
-        return '-'.join((version.split('-')[0], 'nightly'))
-
-def stubfolder(path:str)->str:
-    "retunr path in the stub folder"
-    return '{}/{}'.format(STUB_FOLDER,path)
+STUB_FOLDER = './all-stubs'
 
 def get_all():
-    "get all frozen modules for the current version of micropythin"
+    "get all frozen modules for the current version of micropython"
+
+    get_cpython.get_core(stub_path= stubfolder('cpython_core'), requirements='./src/reqs-cpython-mpy.txt')
 
     #todo: checkout/check specific version of micropython
     mpy_path = '../micropython'
@@ -93,18 +39,16 @@ def get_all():
         # folder/{family}_{version}_frozen
         family = 'mpy'
         stub_path = stubfolder( '{}_{}_frozen'.format(family, flat_version(version)))
-        freezer_mpy.get_frozen(stub_path, mpy_path, lib_path='../micropython-lib')
+        get_mpy.get_frozen(stub_path, mpy_path, lib_path='../micropython-lib')
     else:
         log.warning('Unable to find the micropython repo in folder : {}'.format(mpy_path))
 
-
-    get_cpython(stub_path= stubfolder('cpython_core'), requirements='./src/reqs-cpython-mpy.txt')
-
-    # freezer_lobo.get_frozen(stub_path='./stubs/esp32_LoBo_3_2_24_Frozen')
+    # todo add eralier version(s) tags have been removed ...
+    get_lobo.get_frozen(stub_path=STUB_FOLDER + '/esp32_LoBo_3_2_24_Frozen')
 
     # now generate typeshed files for all scripts
     log.info("Generate type hint files (pyi) in folder: {}".format(STUB_FOLDER))
-    make_stub_files(STUB_FOLDER, levels=7)
+    utils.make_stub_files(STUB_FOLDER, levels=7)
 
 
 if __name__ == "__main__":
---
+++ src/get_cpython.py
@@ -0,0 +1,50 @@
+import os
+import glob
+import shutil
+import subprocess
+import utils
+import json
+
+import logging
+log = logging.getLogger(__name__)
+
+family = 'common'
+VERSION = '1.3.5'
+def get_core(requirements, stub_path=None):
+    "Download MicroPython compatibility modules"
+    if not stub_path:
+        stub_path = './all-stubs/cpython-core'
+
+    # use pip to dowload requirements file to build folder in one go
+    #   pip install --no-compile --no-cache-dir --target ./scratch/test --upgrade -r ./src/micropython.txt
+    build_path = os.path.abspath("./build")
+    os.makedirs(stub_path, exist_ok=True)
+    os.makedirs(build_path, exist_ok=True)
+    try:
+        subprocess.run(["pip", "install", "--target", build_path, "-r", requirements,
+                        "--no-cache-dir", "--no-compile", "--upgrade", "--no-binary=:all:"],
+                        capture_output=False, check=True)
+        # build modules.json
+        mod_manifest = utils.manifest(machine=family, version= VERSION)      
+        # copy *.py files in build folder to stub_path
+        for filename in glob.glob(os.path.join(build_path, "*.py")):
+            log.info("pipped : {}".format(filename))
+            f_name, f_ext = os.path.splitext(os.path.basename(filename))
+            mod_manifest['modules'].append({ "file": os.path.basename(filename), "module":f_name})
+            try:
+                shutil.copy2(filename, stub_path)
+            except OSError as err:
+                log.exception(err)
+    except OSError as err:
+        log.error("An error occurred while trying to run pip to dowload the MicroPython compatibility modules from PyPi: {}".format(err))
+    finally:
+        # remove build folder
+        shutil.rmtree(build_path, ignore_errors=True)
+        #withe the module manifest 
+        with open(stub_path+"/modules.json", "w") as outfile:
+            json.dump(mod_manifest, outfile)
+
+if __name__ == "__main__":
+    # just run a quick test    
+    logging.basicConfig(format='%(levelname)-8s:%(message)s',level=logging.INFO)
+    get_core(requirements='./src/reqs-cpython-mpy.txt', stub_path='./scratch/cpython_common')
