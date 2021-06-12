b08e7360f0a3931316e697024184ccef9c7977b9
Jos Verlinde <josverl@microsoft.com>
2021-06-12 16:26:46 +0200

    black formatter

--- src/get_all_frozen.py
+++ src/get_all_frozen.py
@@ -30,27 +30,36 @@ import get_lobo
 
 log = logging.getLogger(__name__)
 
-STUB_FOLDER = './all-stubs'
+STUB_FOLDER = "./all-stubs"
+
 
 def get_all():
     "get all frozen modules for the current version of micropython"
-    
-    #
-    get_cpython.get_core(stub_path=stubfolder('cpython_core'), requirements='./src/reqs-cpython-mpy.txt')
 
-    mpy_path = '../micropython'
+    #
+    get_cpython.get_core(
+        stub_path=stubfolder("cpython_core"), requirements="./src/reqs-cpython-mpy.txt"
+    )
+
+    mpy_path = "../micropython"
     version = clean_version(git.get_tag(mpy_path))
 
     if version:
         log.info("found micropython version : {}".format(version))
         # folder/{family}-{version}-frozen
-        family = 'micropython'
-        stub_path = stubfolder('{}-{}-frozen'.format(family, flat_version(version)))
-        get_mpy.get_frozen(stub_path, version=version, mpy_path=mpy_path, lib_path='../micropython-lib')
+        family = "micropython"
+        stub_path = stubfolder("{}-{}-frozen".format(family, flat_version(version)))
+        get_mpy.get_frozen(
+            stub_path, version=version, mpy_path=mpy_path, lib_path="../micropython-lib"
+        )
     else:
-        log.warning('Unable to find the micropython repo in folder : {}'.format(mpy_path))
+        log.warning(
+            "Unable to find the micropython repo in folder : {}".format(mpy_path)
+        )
 
-    get_lobo.get_frozen(stub_path=STUB_FOLDER + '/loboris-esp32_lobo_3_2_24' + '-frozen')
+    get_lobo.get_frozen(
+        stub_path=STUB_FOLDER + "/loboris-esp32_lobo_3_2_24" + "-frozen"
+    )
 
     # now generate typeshed files for all scripts
     log.info("Generate type hint files (pyi) in folder: {}".format(STUB_FOLDER))
@@ -58,7 +67,8 @@ def get_all():
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
-                        format='%(name)-10s %(levelname)-8s:%(message)s')
+    logging.basicConfig(
+        level=logging.INFO, format="%(name)-10s %(levelname)-8s:%(message)s"
+    )
 
     get_all()
--- src/get_lobo.py
+++ src/get_lobo.py
@@ -12,31 +12,52 @@ from pathlib import Path
 import downloader
 import utils
 
-FAMILY = 'loboris'
-PORT = 'esp32_lobo'
+FAMILY = "loboris"
+PORT = "esp32_lobo"
 
 
 log = logging.getLogger(__name__)
 # log.setLevel(level=logging.DEBUG)
 
-def get_frozen(stub_path=None, *, repo=None, version='3.2.24'):
+
+def get_frozen(stub_path=None, *, repo=None, version="3.2.24"):
     "Loboris frozen modules"
     if stub_path is None:
-        stub_path = Path('./all-stubs')  / "{}-{}-frozen".format(FAMILY, utils.flat_version(version) )
+        stub_path = Path("./all-stubs") / "{}-{}-frozen".format(
+            FAMILY, utils.flat_version(version)
+        )
     else:
         stub_path = Path(stub_path)
 
     if not repo:
-        repo = 'https://raw.githubusercontent.com/loboris/MicroPython_ESP32_psRAM_LoBo/master/MicroPython_BUILD/components/micropython/esp32/modules/{}'
+        repo = "https://raw.githubusercontent.com/loboris/MicroPython_ESP32_psRAM_LoBo/master/MicroPython_BUILD/components/micropython/esp32/modules/{}"
 
-    frozen_modules = ["README.md", "ak8963.py", "freesans20.py", "functools.py", "logging.py", "microWebSocket.py", "microWebSrv.py", "microWebTemplate.py", "mpu6500.py", "mpu9250.py", "pye.py", "ssd1306.py", "tpcalib.py", "upip.py",
-                      "upip_utarfile.py", "upysh.py", "urequests.py", "writer.py"]
-    #download
+    frozen_modules = [
+        "README.md",
+        "ak8963.py",
+        "freesans20.py",
+        "functools.py",
+        "logging.py",
+        "microWebSocket.py",
+        "microWebSrv.py",
+        "microWebTemplate.py",
+        "mpu6500.py",
+        "mpu9250.py",
+        "pye.py",
+        "ssd1306.py",
+        "tpcalib.py",
+        "upip.py",
+        "upip_utarfile.py",
+        "upysh.py",
+        "urequests.py",
+        "writer.py",
+    ]
+    # download
     downloader.download_files(repo, frozen_modules, stub_path)
     # make a manifest
     utils.make_manifest(stub_path, FAMILY, "frozen", version=version)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(format='%(levelname)-8s:%(message)s',level=logging.INFO)
-    get_frozen(version='3.2.24')
+    logging.basicConfig(format="%(levelname)-8s:%(message)s", level=logging.INFO)
+    get_frozen(version="3.2.24")
