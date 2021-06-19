073fa88acfb845a7e5963af148b9a4c92dbb2f6f
Jos Verlinde <jos_verlinde@hotmail.com>
2021-06-19 21:46:13 +0000

    update dev requirements, add test form minification

--- tests/common/minify_test.py
+++ tests/common/minify_test.py
@@ -1,4 +1,18 @@
+import os
+import subprocess
 
+def test_minifier(tmp_path):
+    "test creation of minified  version"
+    # Use temp_path to generate stubs
+    cmd = ["python", "process.py", "minify"]
+    try:
+        subproc = subprocess.run(cmd, timeout=100000)
+        assert subproc.returncode == 0, "process minify should run without errors"
+
+    except ImportError as e:
+        print(e)
+        pass
+ 
 
 def test_minified_no_log():
     with open('minified/createstubs.py') as f:
