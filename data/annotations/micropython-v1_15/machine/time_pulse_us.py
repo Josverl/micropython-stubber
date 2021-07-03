450988fc308c7084c0f0562cabd242fa48a0b067
Jos Verlinde <josverl@microsoft.com>
2021-07-03 20:00:07 +0200

    first few descriptions

---
+++ data/annotations/micropython-v1_15/machine/time_pulse_us.py
@@ -0,0 +1,19 @@
+# def time_pulse_us():
+# def time_pulse_us(Any) -> Any:
+def time_pulse_us(pin: Pin, pulse_level: int, timeout_us: int = 1000000, /) -> int:
+    """
+    Time a pulse on the given *pin*, and return the duration of the pulse in
+    microseconds.  The *pulse_level* argument should be 0 to time a low pulse
+    or 1 to time a high pulse.
+
+    If the current input value of the pin is different to *pulse_level*,
+    the function first (*) waits until the pin input becomes equal to *pulse_level*,
+    then (**) times the duration that the pin is equal to *pulse_level*.
+    If the pin is already equal to *pulse_level* then timing starts straight away.
+
+    The function will return -2 if there was timeout waiting for condition marked
+    (*) above, and -1 if there was timeout during the main measurement, marked (**)
+    above. The timeout is the same for both cases and given by *timeout_us* (which
+    is in microseconds).
+    """
+    ...
