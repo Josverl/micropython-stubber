0baf6c6744032cbcf50a8cb5e42b5ca7c8c0a471
Jos Verlinde <josverl@microsoft.com>
2021-07-13 01:01:03 +0200

    black

--- generated/micropython/v1_16/gc.py
+++ generated/micropython/v1_16/gc.py
@@ -11,7 +11,7 @@ from typing import Any, Optional, Union, Tuple
 # .. function:: enable()
 def enable() -> Any:
     """
-    Enable automatic garbage collection.
+       Enable automatic garbage collection.
     """
     ...
 
@@ -19,8 +19,8 @@ def enable() -> Any:
 # .. function:: disable()
 def disable() -> Any:
     """
-    Disable automatic garbage collection.  Heap memory can still be allocated,
-    and garbage collection can still be initiated manually using :meth:`gc.collect`.
+       Disable automatic garbage collection.  Heap memory can still be allocated,
+       and garbage collection can still be initiated manually using :meth:`gc.collect`.
     """
     ...
 
@@ -28,7 +28,7 @@ def disable() -> Any:
 # .. function:: collect()
 def collect() -> Any:
     """
-    Run a garbage collection.
+       Run a garbage collection.
     """
     ...
 
@@ -36,7 +36,7 @@ def collect() -> Any:
 # .. function:: mem_alloc()
 def mem_alloc() -> Any:
     """
-    Return the number of bytes of heap RAM that are allocated.
+       Return the number of bytes of heap RAM that are allocated.
     """
     ...
 
@@ -45,8 +45,8 @@ def mem_alloc() -> Any:
 # .. function:: mem_free()
 def mem_free() -> Any:
     """
-    Return the number of bytes of available heap RAM, or -1 if this amount
-    is not known.
+       Return the number of bytes of available heap RAM, or -1 if this amount
+       is not known.
     """
     ...
 
@@ -55,20 +55,20 @@ def mem_free() -> Any:
 # .. function:: threshold([amount])
 def threshold(amount: Optional[Any]) -> Any:
     """
-    Set or query the additional GC allocation threshold. Normally, a collection
-    is triggered only when a new allocation cannot be satisfied, i.e. on an
-    out-of-memory (OOM) condition. If this function is called, in addition to
-    OOM, a collection will be triggered each time after *amount* bytes have been
-    allocated (in total, since the previous time such an amount of bytes
-    have been allocated). *amount* is usually specified as less than the
-    full heap size, with the intention to trigger a collection earlier than when the
-    heap becomes exhausted, and in the hope that an early collection will prevent
-    excessive memory fragmentation. This is a heuristic measure, the effect
-    of which will vary from application to application, as well as
-    the optimal value of the *amount* parameter.
-
-    Calling the function without argument will return the current value of
-    the threshold. A value of -1 means a disabled allocation threshold.
+       Set or query the additional GC allocation threshold. Normally, a collection
+       is triggered only when a new allocation cannot be satisfied, i.e. on an
+       out-of-memory (OOM) condition. If this function is called, in addition to
+       OOM, a collection will be triggered each time after *amount* bytes have been
+       allocated (in total, since the previous time such an amount of bytes
+       have been allocated). *amount* is usually specified as less than the
+       full heap size, with the intention to trigger a collection earlier than when the
+       heap becomes exhausted, and in the hope that an early collection will prevent
+       excessive memory fragmentation. This is a heuristic measure, the effect
+       of which will vary from application to application, as well as
+       the optimal value of the *amount* parameter.
+    
+       Calling the function without argument will return the current value of
+       the threshold. A value of -1 means a disabled allocation threshold.
     """
     ...
 
