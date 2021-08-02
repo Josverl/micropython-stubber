83a07962747b45e1692deca8fab80c194a3cacaa
Jos Verlinde <josverl@microsoft.com>
2021-08-02 22:40:57 +0200

    add module name to return type lookup table

--- tests/rst_reader/test_returns.py
+++ tests/rst_reader/test_returns.py
@@ -9,7 +9,7 @@ from rst_utils import type_from_docstring
 
 ### Test setup
 @pytest.mark.parametrize(
-    "sig, docstr, exp, conf",
+    "signature, docstring, expected_type, confidence",
     [
         (".. function:: heap_unlock()", "", "Any", 0),
         (".. function:: heap_unlock()->None", "", "None", 1),
@@ -20,30 +20,37 @@ from rst_utils import type_from_docstring
         #        (".. class:: heap_unlock()->str : ", "", "None", 1),
     ],
 )
-def test_signatures(sig, docstr, exp, conf):
+def test_signatures(signature, docstring, expected_type, confidence):
     # return type should be included in the signature
     # except for classes
-    r = type_from_docstring(docstr, sig)
-    assert r["type"] == exp
-    assert r["confidence"] >= conf
+    r = type_from_docstring(docstring, signature, "builtins")
+    assert r["type"] == expected_type
+    assert r["confidence"] >= confidence
 
 
 # read the tests cases from a json file to avoid needing to code all the different tests
-def return_type_testcases() -> List[Tuple[str, str, str, int]]:
+def return_type_testcases() -> List[Tuple[str, str, str, str, int]]:
     filename = Path("./tests/rst_reader/data/return_testcases.json")
     doc = []
     with open(filename, encoding="utf8") as fp:
         doc = json.load(fp)
     cases = []
     for tc in doc:
-        cases.append((tc["signature"], tc["docstring"], tc["type"], tc["confidence"]))
+        try:
+            cases.append(
+                (tc["module"], tc["signature"], tc["docstring"], tc["type"], tc["confidence"])
+            )
+        except KeyError:
+            print("INVALID TEST DATA ERROR", tc)
     return cases
 
 
-@pytest.mark.parametrize("sig, docstr, exp, conf", return_type_testcases())
-def test_returns(sig, docstr, exp, conf):
+@pytest.mark.parametrize(
+    "module, signature, docstring, expected_type, confidence", return_type_testcases()
+)
+def test_returns(module, signature, docstring, expected_type, confidence):
     # return type should be included in the signature
     # except for classes
-    r = type_from_docstring(docstr, sig)
-    assert r["type"] == exp
-    assert r["confidence"] >= conf
+    r = type_from_docstring(docstring, signature, module)
+    assert r["type"] == expected_type
+    assert r["confidence"] >= confidence
