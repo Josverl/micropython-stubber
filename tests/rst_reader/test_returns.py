5cb566e7914f0e02c736db8a1fae999b46f1452d
Jos Verlinde <josverl@microsoft.com>
2021-08-02 17:27:42 +0200

    add test cases for return types both common sanity checks and to be improved

--- tests/rst_reader/test_returns.py
+++ tests/rst_reader/test_returns.py
@@ -1,11 +1,13 @@
-from typing import Dict, List
+from typing import Dict, List, Tuple
 import pytest
 from pathlib import Path
+import json
 
 # SOT
 from rst_utils import type_from_docstring
 
 
+### Test setup
 @pytest.mark.parametrize(
     "sig, docstr, exp, conf",
     [
@@ -19,6 +21,27 @@ from rst_utils import type_from_docstring
     ],
 )
 def test_signatures(sig, docstr, exp, conf):
+    # return type should be included in the signature
+    # except for classes
+    r = type_from_docstring(docstr, sig)
+    assert r["type"] == exp
+    assert r["confidence"] >= conf
+
+
+# read the tests cases from a json file to avoid needing to code all the different tests
+def return_type_testcases() -> List[Tuple[str, str, str, int]]:
+    filename = Path("./tests/rst_reader/data/return_testcases.json")
+    doc = []
+    with open(filename, encoding="utf8") as fp:
+        doc = json.load(fp)
+    cases = []
+    for tc in doc:
+        cases.append((tc["signature"], tc["docstring"], tc["type"], tc["confidence"]))
+    return cases
+
+
+@pytest.mark.parametrize("sig, docstr, exp, conf", return_type_testcases())
+def test_returns(sig, docstr, exp, conf):
     # return type should be included in the signature
     # except for classes
     r = type_from_docstring(docstr, sig)
