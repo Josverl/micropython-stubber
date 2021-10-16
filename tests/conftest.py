18aae94582b7290317e4008355a434e20a185750
Jos Verlinde <jos_verlinde@hotmail.com>
2021-10-16 23:27:45 +0200

    remove filelock as  parallel testing is disabled --> way too many issues introduced by this

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ Shared Test Fixtures
 """
 import sys
 import pytest
+# from filelock import FileLock
 
 # make sure that the source can be found
 sys.path.insert(1, "./src")
@@ -16,9 +17,43 @@ TESTLIB = "./micropython-lib"
 def testrepo_micropython_lib():
     "get exclusive access to micropython-lib repo to prevent multiple tests from interfering with each other"
     return TESTLIB
+    # disabled parallel testing --> way too many issues introduced by this
+
 
 
 @pytest.fixture(scope="session")
 def testrepo_micropython():
     "get exclusive access to micropython repo to prevent multiple tests from interfering with each other"
     return TESTREPO
+    # disabled parallel testing --> way too many issues introduced by this
+    # if not worker_id:
+    #     return foldername
+    #     # not executing in with multiple workers, just go for it
+
+    # # get the temp directory shared by all workers
+    # root_tmp_dir = tmp_path_factory.getbasetemp().parent
+
+    # fn = root_tmp_dir / "micropython.repo"
+    # with FileLock(str(fn) + ".lock"):
+    #     # yield to run the test
+    #     yield foldername
+
+
+@pytest.fixture(scope="session")
+def gitrepo_this(tmp_path_factory, worker_id):
+    "get exclusive access to this repo"
+    return ".git"
+    # disabled parallel testing --> way too many issues introduced by this
+
+    # if not worker_id:
+    #     # not executing in with multiple workers, just go for it
+    #     return ".git"
+    #     # disabled parallel testing --> way too many issues introduced by this
+
+    # # get the temp directory shared by all workers
+    # root_tmp_dir = tmp_path_factory.getbasetemp().parent
+
+    # fn = root_tmp_dir / "this.repo"
+    # with FileLock(str(fn) + ".lock"):
+    #     # yield to run the test
+    #     yield ".git"
