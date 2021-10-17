b9288f76e0d44050e312d318e705e3c4c180007b
Jos Verlinde <josverl@microsoft.com>
2021-10-17 17:19:54 +0200

    rename stubtest folder

---
+++ tests/data/stubs-ok/micropython-linux-1_12/ucollections.py
@@ -0,0 +1,53 @@
+"""
+Module: 'ucollections' on micropython-linux-1.12
+"""
+# MCU: {'ver': '1.12', 'port': 'linux', 'arch': 'x64', 'sysname': 'unknown', 'release': '1.12.0', 'name': 'micropython', 'mpy': 2821, 'version': '1.12.0', 'machine': 'unknown', 'build': '', 'nodename': 'unknown', 'platform': 'linux', 'family': 'micropython'}
+# Stubber: 1.3.6
+
+class OrderedDict:
+    ''
+    def clear():
+        pass
+
+    def copy():
+        pass
+
+    def fromkeys():
+        pass
+
+    def get():
+        pass
+
+    def items():
+        pass
+
+    def keys():
+        pass
+
+    def pop():
+        pass
+
+    def popitem():
+        pass
+
+    def setdefault():
+        pass
+
+    def update():
+        pass
+
+    def values():
+        pass
+
+
+class deque:
+    ''
+    def append():
+        pass
+
+    def popleft():
+        pass
+
+def namedtuple():
+    pass
+
