c4ec260423133b71ea71550ee59ab842defd2d7e
Jos Verlinde <josverl@microsoft.com>
2021-10-17 19:51:53 +0200

    fix test and rename test data

---
+++ tests/data/stubs-issues/tst-micropython-linux-1_13/collections.py
@@ -0,0 +1,59 @@
+"""
+Module: 'collections' on micropython-linux-1.13
+"""
+# MCU: {'ver': '1.13', 'port': 'linux', 'arch': 'x64', 'sysname': 'unknown', 'release': '1.13.0', 'name': 'micropython', 'mpy': 2821, 'version': '1.13.0', 'machine': 'unknown', 'build': '', 'nodename': 'unknown', 'platform': 'linux', 'family': 'micropython'}
+# Stubber: 1.3.7
+
+class OrderedDict:
+    def __init__(self):
+        ''
+        pass
+
+    def clear():
+        pass
+
+    def copy():
+        pass
+
+    def fromkeys():
+        pass
+
+    def get():
+        pass
+
+    def items():
+        pass
+
+    def keys():
+        pass
+
+    def pop():
+        pass
+
+    def popitem():
+        pass
+
+    def setdefault():
+        pass
+
+    def update():
+        pass
+
+    def values():
+        pass
+
+
+class deque:
+    def __init__(self):
+        ''
+        pass
+
+    def append():
+        pass
+
+    def popleft():
+        pass
+
+def namedtuple():
+    pass
+
---
+++ tests/data/stubs-issues/tst-micropython-linux-1_13/machine.py
@@ -0,0 +1,58 @@
+"""
+Module: 'machine' on micropython-linux-1.13
+"""
+# MCU: {'ver': '1.13', 'port': 'linux', 'arch': 'x64', 'sysname': 'unknown', 'release': '1.13.0', 'name': 'micropython', 'mpy': 2821, 'version': '1.13.0', 'machine': 'unknown', 'build': '', 'nodename': 'unknown', 'platform': 'linux', 'family': 'micropython'}
+# Stubber: 1.3.7
+from typing import Any
+
+print( 'Hello bowler')
+
+def on():
+    i = 1 
+    pass
+
+class PinBase:
+    def __init__(self):
+        ''
+        pass
+
+
+
+class Foo:
+    def __init__(self):
+        ''
+        pass
+
+    def off():
+        pass
+
+    def on():
+        pass
+
+    def value():
+        pass
+
+
+class Signal:
+    def __init__(self):
+        ''
+        pass
+
+    def off():
+        pass
+
+    def on(self, twee, twee, twee, twee):
+        pass
+
+    def value():
+        pass
+
+def idle():
+    pass
+
+mem16 = None
+mem32 = None
+mem8 = None
+def time_pulse_us():
+    pass
+
