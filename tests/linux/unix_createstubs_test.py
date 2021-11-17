f5c199190090d969dbbe1ab2f1c0de5fef5f5625
Jos Verlinde <jos.verlinde@hotmail.com>
2021-11-17 21:09:58 +0100

    test: linux detection++

--- tests/linux/unix_createstubs_test.py
+++ tests/linux/unix_createstubs_test.py
@@ -6,34 +6,31 @@ import sys
 import subprocess
 from pathlib import Path
 import pytest
+import distro
 
 
+ubuntu_version = "?"
+fw_list = []
 # Figure out ubuntu version
-try:
-    import lsb_release
-
-    ubuntu_version = lsb_release.get_os_release()["RELEASE"]
-    ubuntu_name = lsb_release.get_os_release()["CODENAME"]
-except Exception:
-    ubuntu_name = "focal"
-    ubuntu_version = ""
+if sys.platform == "linux":
+    if distro.id() == "ubuntu":
+        ubuntu_version = distro.version()
 
 
-if ubuntu_name == "focal":
-    # 20.04
+# Default = 20.04 - focal
+fw_list = [
+    "ubuntu_20_04/micropython_v1_11",
+    "ubuntu_20_04/micropython_v1_12",
+    "ubuntu_20_04/micropython_v1_14",
+    "ubuntu_20_04/micropython_v1_15",
+    "ubuntu_20_04/micropython_v1_16",
+]
+if ubuntu_version == "18.04":
+    # 18.04 - bionic
     fw_list = [
-        "micropython_v1_11",
-        "micropython_v1_12",
-        "micropython_v1_14",
-        "micropython_v1_15",
-        "micropython_v1_16",
-    ]
-elif ubuntu_name == "bionic":
-    # 18.04 ?:
-    fw_list = [
-        "micropython_1_12",
-        "micropython_1_13",
-        "pycopy_3_3_2-25",
+        "ubuntu_18_04/micropython_1_12",
+        "ubuntu_18_04/micropython_1_13",
+        "ubuntu_18_04/pycopy_3_3_2-25",
     ]
 
 
@@ -54,7 +51,7 @@ elif ubuntu_name == "bionic":
 def test_createstubs(firmware, tmp_path: Path, script_folder):
     # Use temp_path to generate stubs
     scriptfolder = os.path.abspath(script_folder)
-    cmd = [os.path.abspath("tests/tools/ubuntu_20_04/" + firmware), "createstubs.py", "--path", str(tmp_path)]
+    cmd = [os.path.abspath("tests/tools/" + firmware), "createstubs.py", "--path", str(tmp_path)]
     try:
         subproc = subprocess.run(
             cmd,
