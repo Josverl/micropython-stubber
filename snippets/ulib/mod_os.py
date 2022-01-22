5f25baadd71b044ba195816ce7f8db0d7a88021e
Jos Verlinde <jos.verlinde@microsoft.com>
2022-01-22 21:54:53 +0100

    add snippets to test mpy stdlib

--- snippets/ulib/mod_os.py
+++ snippets/ulib/mod_os.py
@@ -1,7 +1,7 @@
 # SD card
 import machine, os
 # Slot 2 uses pins sck=18, cs=5, miso=19, mosi=23
-sd = machine.SDCard(slot=2)
+# sd = machine.SDCard(slot=2)
 os.mount(sd, "/sd")  # mount
 
 os.listdir('/sd')    # list directory contents
