a577bc437161cfffd025a81263ec982145b12bd6
Jos Verlinde <jos.verlinde@microsoft.com>
2022-01-31 20:57:36 +0000

    Multi-root with mpy-stdlib based on pico-go

--- snippets/ulib/mod_os.py
+++ snippets/ulib/mod_os.py
@@ -1,10 +1,15 @@
 # SD card
-import machine, os
+import os
+
 # Slot 2 uses pins sck=18, cs=5, miso=19, mosi=23
 # sd = machine.SDCard(slot=2)
+sd = 1
 os.mount(sd, "/sd")  # mount
 
-os.listdir('/sd')    # list directory contents
+os.listdir("/sd")  # list directory contents
 
-os.umount('/sd')     # eject
+os.umount("/sd")  # eject
+
+
+import uos 
 
