882d8cf2697052c1973fdb3889b17d281197804d
Jos Verlinde <jos.verlinde@microsoft.com>
2022-01-31 19:37:51 +0000

    devcontainer: change python setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,7 +1,15 @@
-#!/bin/bash
-# initialize the submodules
-git submodule init
-git submodule update
-
-# initialize the python requirements
-pip3 install --user -r requirements-dev.txt
+#!/usr/bin/env bash
+# this script is referenced from the codespaces devcontainer dockerfile
+# git submodule init
+# git submodule update
+#
+if [ "${CODESPACES}" = "true" ]; then
+    # create & activate venv and install the dependencies in there 
+    python -m venv .venv-cs
+    source .venv-cs/bin/activate
+else
+    python3 -m venv .venv-ub
+    source .venv-ub/bin/activate
+fi
+# python -m pip install --upgrade pip
+python -m pip install -r requirements-dev.txt
