a82a64bad21ef18f5a5551f3b72c639dd7fde763
Jos Verlinde <jos_verlinde@hotmail.com>
2022-01-31 20:22:23 +0000

    minify.ps1: posix paths

--- scripts/minify.ps1
+++ scripts/minify.ps1
@@ -1,6 +1,6 @@
 # Minify normal version
 python process.py minify -xc
 # Minify low_mem version
-python .\process.py minify -s board\createstubs_mem.py -o minified -xc
+python process.py minify -s board/createstubs_mem.py -o minified -xc
 # Minify low_mem _ restartable version
-python .\process.py minify -s board\createstubs_db.py -o minified -xc
+python process.py minify -s board/createstubs_db.py -o minified -xc
