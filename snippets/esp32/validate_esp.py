29aba1c308076fef9488d6fda49cc1a45dfb1f6e
Jos Verlinde <jos.verlinde@microsoft.com>
2022-02-22 13:47:23 +0100

    update snippets

--- snippets/esp32/validate_esp.py
+++ snippets/esp32/validate_esp.py
@@ -19,12 +19,11 @@ esp.flash_write(byte_offset, buffer)
 esp.flash_read(byte_offset, buffer)
 
 # Neopixel 
+# # Note: ESP8266 only 
 
-import esp
-# For low-level driving of a NeoPixel:
-pin = Pin(18)
-grb_buf = (1, 20, 2, 40)
-is800khz = False
-
-# Note: ESP8266 only 
-esp.neopixel_write(pin, grb_buf, is800khz)  # type: ignore
+# import esp
+# # For low-level driving of a NeoPixel:
+# pin = Pin(18)
+# grb_buf = (1, 20, 2, 40)
+# is800khz = False
+# esp.neopixel_write(pin, grb_buf, is800khz)  # type: ignore
