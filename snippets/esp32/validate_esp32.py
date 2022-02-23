2141115303b7801f7d920302e3d732d010ddc68e
Jos Verlinde <jos.verlinde@microsoft.com>
2022-02-23 08:48:15 +0100

    snippets:add checks

--- snippets/esp32/validate_esp32.py
+++ snippets/esp32/validate_esp32.py
@@ -20,10 +20,12 @@ r.write_pulses((1, 20, 2, 40), 0)  # Send 0 for 100ns, 1 for 2000ns, 0 for 200ns
 
 ## esp32/modules/flashbdev.py
 
-from esp32 import Partition
+# TODO: Partition does not seem to be handled correctly 
+# is this code snippet correct ?
 
-# MicroPython's partition table uses "vfs", TinyUF2 uses "ffat".
-bdev = Partition.find(Partition.TYPE_DATA, label="vfs")
-if not bdev:
-    bdev = Partition.find(Partition.TYPE_DATA, label="ffat")
-bdev = bdev[0] if bdev else None
+# from esp32 import Partition
+# # MicroPython's partition table uses "vfs", TinyUF2 uses "ffat".
+# bdev = Partition.find(Partition.TYPE_DATA, label="vfs")
+# if not bdev:
+#     bdev = Partition.find(Partition.TYPE_DATA, label="ffat")
+# bdev = bdev[0] if bdev else None
