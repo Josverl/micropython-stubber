7d92d519cc1b363dbf842f9a15d1a201a25ad730
Jos Verlinde <jos.verlinde@microsoft.com>
2022-02-26 14:04:34 +0100

    cli: refactor minify and add -all option

--- tests/integration/minify_test.py
+++ tests/integration/minify_test.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import subprocess
 import pytest
 
+import stubber.minify as minify
+
 
 @pytest.mark.parametrize("source", ["createstubs.py", "createstubs_mem.py", "createstubs_db.py"])
 @pytest.mark.slow
@@ -10,15 +12,9 @@ def test_minification_py(tmp_path: Path, source: str):
     "python script - test creation of minified version"
     # load process.py in the same python environment
     source_path = Path("./board") / source
-    # cmd = [sys.executable, "process.py", "minify", "--source", source_path.as_posix(), "--target", tmp_path.as_posix()]
-    cmd = [sys.executable, "src/stubber/process.py", "minify", "--source", source_path.as_posix(), "--target", tmp_path.as_posix()]
-    try:
-        subproc = subprocess.run(cmd, timeout=100000)
-        assert subproc.returncode == 0, "process minify should run without errors"
 
-    except ImportError as e:
-        print(e)
-        pass
+    result = minify.minify(source=source_path, target=tmp_path)
+    assert result == 0
     # now test that log statements have been removed
     with open(tmp_path / source) as f:
         content = f.readlines()
