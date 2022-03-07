5b8dd1a7fefc44d5fe9f88f80a12e98c8aba92f0
Jos Verlinde <jos.verlinde@microsoft.com>
2022-03-07 18:15:25 +0100

    test: change coverage reporting to codecov only

--- scripts/coverage-local.ps1
+++ scripts/coverage-local.ps1
@@ -4,4 +4,11 @@ coverage xml
 coverage json
 coverage lcov
 coverage html
+
 start coverage\index.html
+
+# Just a link to codecov
+testspace tests-results.xml --link=codecov
+
+\develop\tools\codecov.exe -t ${CODECOV_TOKEN} -s ./results
+\develop\tools\testspace.exe [dev]results/test-results*.xml --link codecov
