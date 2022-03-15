dce23c760cae8b638df4afe91845b8d85604dca3
Jos Verlinde <jos.verlinde@microsoft.com>
2022-03-15 10:41:35 +0100

    cover: fix pragma no cover

--- src/stubber/__init__.py
+++ src/stubber/__init__.py
@@ -1,13 +1,13 @@
 """read the version from pyproject or the wheels"""
 try:
     import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:  # pragma : nocover
+except ModuleNotFoundError:  # pragma : no cover
     import importlib_metadata  # type: ignore
 
 try:
     # todo: use __name__
     __version__ = importlib_metadata.version("micropython-stubber")
-except Exception:  # pragma : nocover
+except Exception:  # pragma : no cover
     __version__ = "0.0.0"
 
 from . import utils
