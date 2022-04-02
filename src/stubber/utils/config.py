d335ab55d9b4b7ba6d08c8f47a4fe0dc7261273f
Jos Verlinde <jos.verlinde@microsoft.com>
2022-04-02 23:52:04 +0200

    use config and config docs

--- src/stubber/utils/config.py
+++ src/stubber/utils/config.py
@@ -10,23 +10,26 @@ log = logging.getLogger(__name__)
 class StubberConfig(Config):
     "stubber confguration class"
     stub_path = key(key_name="stub-path", cast=Path, required=False, default=Path("./stubs"))
-    # relative to stub
+    "a Path to the stubs directory" 
+    # relative to stubs folder
     fallback_path = key(key_name="fallback-path", cast=Path, required=False, default=Path("typings/fallback"))
 
     repo_path = key(key_name="repo-path", cast=Path, required=False, default=Path("./repo"))
-    # relative to repo
-    mpy_path = key(key_name="mpy-path", cast=Path, required=False, default=Path("./micropython"))
-    mpy_lib_path = key(key_name="mpy-lib-path", cast=Path, required=False, default=Path("./micropython-lib"))
+    "a Path to the repo directory"
+    # micropython and micropython-lib are relative to ./repo folder
+    mpy_path = key(key_name="mpy-path", cast=Path, required=False, default=Path("micropython"))
+    "a Path to the micropython dolder in the repo directory"
+    mpy_lib_path = key(key_name="mpy-lib-path", cast=Path, required=False, default=Path("micropython-lib"))
+    "a Path to the micropython=lib folder in the repo directory"
 
     def post_read_hook(self) -> dict:
         config_updates = dict()
-        # relative to repo path
-        config_updates.update(mpy_path=self.repo_path / self.mpy_path)
-        config_updates.update(mpy_lib_path=self.repo_path / self.mpy_lib_path)
-
         # relative to stubs
         config_updates.update(fallback_path=self.stub_path / self.fallback_path)
 
+        # relative to repo path
+        config_updates.update(mpy_path=self.repo_path / self.mpy_path)
+        config_updates.update(mpy_lib_path=self.repo_path / self.mpy_lib_path)
         return config_updates
 
 
