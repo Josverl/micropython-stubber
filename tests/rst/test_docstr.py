5561a3ea12d79baf569cf6b943d23f728233df88
Jos Verlinde <jos.verlinde@microsoft.com>
2022-05-08 23:47:22 +0200

    test: add long method test

--- tests/rst/test_docstr.py
+++ tests/rst/test_docstr.py
@@ -94,3 +94,47 @@ def test_parse_docstr_quoted():
 
     assert not any([l.startswith("# :") for l in r.output_dict["constants"]]), "Some lines were not unquoted"
     assert not any([l.startswith("# +") for l in r.output_dict["constants"]]), "Some lines were not unquoted"
+
+
+## method is broken over two lines
+PYB_CAN_DOCSTR = """
+.. currentmodule:: pyb
+.. _pyb.CAN:
+
+class CAN -- controller area network communication bus
+======================================================
+
+CAN implements support for classic CAN (available on F4, F7 MCUs) and CAN FD (H7 series) controllers.
+
+The following CAN module functions and their arguments are available
+for both classic and FD CAN controllers, unless otherwise stated.
+
+Constructors
+------------
+
+.. class:: pyb.CAN(bus, ...)
+
+   Construct a CAN object on the given bus.  *bus* can be 1-2, or ``'YA'`` or ``'YB'``.
+
+Methods
+-------
+
+.. method:: CAN.init(mode, prescaler=100, *, sjw=1, bs1=6, bs2=8, auto_restart=False, baudrate=0, sample_point=75,
+        num_filter_banks=14, brs_sjw=1, brs_bs1=8, brs_bs2=3, brs_baudrate=0, brs_sample_point=75)
+
+   Initialise the CAN bus with the given parameters:
+
+"""
+
+
+def test_parse_long_method():
+    r = RSTReader()
+    load_rst(r, PYB_CAN_DOCSTR)
+    r.parse()
+    # check
+    assert len(r.output) > 1
+    assert r.output_dict
+    assert r.output_dict["class CAN():"]
+    assert r.output_dict["class CAN():"]["def init"]
+    assert ")" in r.output_dict["class CAN():"]["def init"]["def"]
+    assert r.output_dict["class CAN():"]["def init"]["def"] == "    def init(self,  -> Any:"
