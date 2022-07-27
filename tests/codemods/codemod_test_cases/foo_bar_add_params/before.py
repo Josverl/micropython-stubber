b52596ebe4e94089ad580913301d240f1f20af45
Jos Verlinde <jos.verlinde@microsoft.com>
2022-07-27 23:18:53 +0200

    add test for classes

---
+++ tests/codemods/codemod_test_cases/foo_bar_add_params/before.py
@@ -0,0 +1,12 @@
+"""
+simple functions and classes
+"""
+
+
+def foo():
+    pass
+
+
+class Bar:
+    def foo():
+        return 1
---
+++ tests/codemods/codemod_test_cases/foo_bar_add_params/stub.py
@@ -0,0 +1,12 @@
+"""
+simple functions 
+"""
+
+
+def foo(pin: int, /, limit: int = 100) -> str:
+    ...
+
+
+class Bar:
+    def foo(y: int, x: int = 10) -> int:
+        return 1
