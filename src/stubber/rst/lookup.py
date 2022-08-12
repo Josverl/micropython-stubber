4b17c0c951c51ca0b1e176bc1b0957d34e8df3f4
Jos Verlinde <jos.verlinde@microsoft.com>
2022-08-12 21:11:38 +0200

    fix: docstub for network.LAN.__init__

--- src/stubber/rst/lookup.py
+++ src/stubber/rst/lookup.py
@@ -220,6 +220,9 @@ PARAM_FIXES = [
         ", value, drive, alt",
         ", value=None, drive=0, alt=-1",
     ),
+    # network.LAN.init
+    # def __init__(self, id, *, phy_type=<board_default>, phy_addr=<board_default>, phy_clock=<board_default>) -> None:
+    ("=<board_default>", "=0"),
 ]
 
 # List of classes and their parent classes that should be added to the class definition
