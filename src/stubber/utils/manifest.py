df5950059ff627bf42cedf416207fef528e81d2a
Jos Verlinde <jos.verlinde@microsoft.com>
2022-08-22 11:22:04 +0200

    fix: refactor __version__

--- src/stubber/utils/manifest.py
+++ src/stubber/utils/manifest.py
@@ -2,7 +2,7 @@ import json
 import logging
 
 from pathlib import Path
-from .. import __version__
+from .my_version import __version__
 from typing import Optional
 from .versions import clean_version
 
