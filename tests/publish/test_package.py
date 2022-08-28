204bb18f9e4d65f1e45fb439ae55e358b36712ee
Jos Verlinde <jos.verlinde@microsoft.com>
2022-08-28 09:27:22 +0200

    fix: package path
    drop path prefixes , no longer user publish/ and stub/ prefixes
    use mocker.path to update location while testing

--- tests/publish/test_package.py
+++ tests/publish/test_package.py
@@ -1,19 +1,11 @@
 import shutil
-
 from pathlib import Path
 
 import pytest
 import stubber.publish.stubpacker as stubpacker
-from stubber.publish.publish_stubs import (
-    ALL_TYPES,
-    COMBO_STUBS,
-    CORE_STUBS,
-    DOC_STUBS,
-    create_package,
-    get_package_info,
-    package_name,
-)
-
+from stubber.publish.publish_stubs import (ALL_TYPES, COMBO_STUBS, CORE_STUBS,
+                                           DOC_STUBS, create_package,
+                                           get_package_info, package_name)
 
 # # use our test paths
 # stubpacker.PUBLISH_PATH = Path("./scratch/publish")
@@ -39,8 +31,6 @@ def test_package_name(family, pkg, port, board, expected):
     assert x == expected
 
 
-
-
 # test creating a DOC_STUBS package
 @pytest.mark.parametrize(
     "pkg_type, port, board",
@@ -50,25 +40,26 @@ def test_package_name(family, pkg, port, board, expected):
     ],
 )
 # CORE_STUBS
-def test_create_package(tmp_path, pytestconfig, pkg_type, port, board):
+def test_create_package(tmp_path, pytestconfig, pkg_type, port, board, mocker):
     """ "
     test Create a new package with the DOC_STUBS type
     - test the different methods to manipulate the package on disk
     """
-    # allow stubpacker to use the test templates
-    stubpacker.TEMPLATE_PATH = pytestconfig.rootpath / "tests/publish/data/template"
 
-    # test data
+    PUBLISH_PATH = tmp_path / "publish"
+    PUBLISH_PATH.mkdir(parents=True)
+
+    # TODO: need to ensure that the stubs are avaialble in GHA testing
+    STUB_PATH = Path("./repos/micropython-stubs/stubs")
+    TEMPLATE_PATH = pytestconfig.rootpath / "tests/publish/data/template"
+
+    mocker.patch("stubber.publish.stubpacker.STUB_PATH", STUB_PATH)
+    mocker.patch("stubber.publish.stubpacker.TEMPLATE_PATH", TEMPLATE_PATH)
+    mocker.patch("stubber.publish.stubpacker.PUBLISH_PATH", PUBLISH_PATH)
+
+    # copy test data ?
     source = pytestconfig.rootpath / "tests/publish/data"
-    root_path = tmp_path
-    dest = root_path / "publish"
-    shutil.copytree(source, dest)
-
-    # TODO: need to endure that the stubs are avaialble in GHA testing
-    stubpacker.ROOT_PATH = root_path
-    stubpacker.STUB_PATH = pytestconfig.rootpath
-    # stubpacker.PUBLISH_PATH = publish_path
-    # stubpacker.TEMPLATE_PATH = pytestconfig.rootpath / "tests/publish/data/template"
+    #    shutil.copytree(source, PUBLISH_PATH)
 
     mpy_version = "v1.18"
     family = "micropython"
@@ -84,39 +75,109 @@ def test_create_package(tmp_path, pytestconfig, pkg_type, port, board):
         # stub_source="./all-stubs",  # for debugging
     )
     assert isinstance(package, stubpacker.StubPackage)
-    run_common_package_tests(package, pkg_name, root_path, pkg_type)
+    run_common_package_tests(package, pkg_name, publish_path=PUBLISH_PATH, stub_path=STUB_PATH, pkg_type=pkg_type)
 
 
-def run_common_package_tests(package, pkg_name, root_path: Path, pkg_type):
+read_db_data = [
+    {
+        "name": "foo-bar-stubs",
+        "mpy_version": "1.18",
+        "publish": True,
+        "pkg_version": "1.18.post6",
+        "path": "foo-v1_18-bar-stubs",
+        "stub_sources": [
+            ["Firmware stubs", "micropython-v1_17-stm32"],
+            ["Frozen stubs", "micropython-v1_17-frozen/stm32/GENERIC"],
+            ["Core Stubs", "cpython_core-pycopy"],
+        ],
+        "description": "foo bar stubs",
+        "hash": "b09f9c819c9e98cbd9dfbc8158079146587e2d66",
+    },
+    {
+        "name": "foo-bar-stubs",
+        "mpy_version": "1.18",
+        "publish": True,
+        "pkg_version": "1.18.post6",
+        "path": "foo-v1_18-bar-stubs",
+        "stub_sources": [
+            ["Firmware stubs", "stubs/micropython-v1_17-stm32"],
+            ["Frozen stubs", "stubs/micropython-v1_17-frozen/stm32/GENERIC"],
+            ["Core Stubs", "stubs/cpython_core-pycopy"],
+        ],
+        "description": "foo bar stubs",
+        "hash": "b09f9c819c9e98cbd9dfbc8158079146587e2d66",
+    },
+    {
+        "name": "foo-bar-stubs",
+        "mpy_version": "1.18",
+        "publish": True,
+        "pkg_version": "1.18.post6",
+        "path": "publish/foo-v1_18-bar-stubs",
+        "stub_sources": [
+            ["Firmware stubs", "micropython-v1_17-stm32"],
+        ],
+        "description": "foo bar stubs",
+        "hash": "b09f9c819c9e98cbd9dfbc8158079146587e2d66",
+    },
+]
+
+
+@pytest.mark.parametrize("json", read_db_data)
+def test_package_from_json(tmp_path, pytestconfig, mocker, json):
+    # test data
+    source = pytestconfig.rootpath / "tests/publish/data"
+    PUBLISH_PATH = tmp_path / "publish"
+    PUBLISH_PATH.mkdir(parents=True)
+
+    # TODO: need to ensure that the stubs are avaialble in GHA testing
+    STUB_PATH = Path("./repos/micropython-stubs/stubs")
+    TEMPLATE_PATH = pytestconfig.rootpath / "tests/publish/data/template"
+
+    mocker.patch("stubber.publish.stubpacker.STUB_PATH", STUB_PATH)
+    mocker.patch("stubber.publish.stubpacker.TEMPLATE_PATH", TEMPLATE_PATH)
+    mocker.patch("stubber.publish.stubpacker.PUBLISH_PATH", PUBLISH_PATH)
+
+    mpy_version = "v1.18"
+    family = "micropython"
+    pkg_name = "foo-bar-stubs"
+    # todo: include stubs in the test data
+    # note uses `stubs` relative to the stubs_folder
+
+    package = stubpacker.StubPackage(pkg_name, version=mpy_version, json_data=json)
+    assert isinstance(package, stubpacker.StubPackage)
+    run_common_package_tests(package, pkg_name, PUBLISH_PATH, stub_path=STUB_PATH, pkg_type=None)
+
+
+def run_common_package_tests(package, pkg_name, publish_path: Path, stub_path: Path, pkg_type):
     "a series of tests to re-use for all packages"
     assert isinstance(package, stubpacker.StubPackage)
     assert package.package_name == pkg_name
 
-    assert not package.package_path.is_absolute(), "package path must be relative to publish folder"
-    assert package.package_path.parent.name == "publish"
-    assert (root_path / package.package_path).exists()
+    assert package.package_path.relative_to(publish_path), "package path should be relative to publish path"
+    assert (package.package_path).exists()
+    assert (package.package_path / "pyproject.toml").exists()
 
     assert len(package.stub_sources) >= 1
     for s in package.stub_sources:
-        assert s[1].is_dir(), "stub source should be folder"
-        assert s[1].exists(), "stubs source should exists"
-        assert not s[1].is_absolute(), "should be a relative path"
-    assert (root_path / package.package_path / "pyproject.toml").exists()
+        folder = stub_path / s[1]
+        assert folder.is_dir(), "stub source should be folder"
+        assert folder.exists(), "stubs source should exists"
+    #        assert not s[1].is_absolute(), "should be a relative path"
     # update existing pyproject.toml
     package.create_update_pyproject_toml()
-    assert (root_path / package.package_path / "pyproject.toml").exists()
+    assert (package.package_path / "pyproject.toml").exists()
 
     package.create_readme()
-    assert (root_path / package.package_path / "README.md").exists()
+    assert (package.package_path / "README.md").exists()
     package.create_license()
-    assert (root_path / package.package_path / "LICENSE.md").exists()
+    assert (package.package_path / "LICENSE.md").exists()
     package.copy_stubs()
-    filelist = list((root_path / package.package_path).rglob("*.py")) + list((root_path / package.package_path).rglob("*.pyi"))
+    filelist = list((package.package_path).rglob("*.py")) + list((package.package_path).rglob("*.pyi"))
     assert len(filelist) >= 1
 
     # do it all at once
     package.update_package_files()
-    filelist = list((root_path / package.package_path).rglob("*.py")) + list((root_path / package.package_path).rglob("*.pyi"))
+    filelist = list((package.package_path).rglob("*.py")) + list((package.package_path).rglob("*.pyi"))
     assert len(filelist) >= 1
 
     package.update_included_stubs()
@@ -138,50 +199,10 @@ def run_common_package_tests(package, pkg_name, root_path: Path, pkg_type):
 
     built = package.build()
     assert built
-    assert (root_path / package.package_path / "dist").exists(), "Distribution folder should exist"
-    filelist = list((root_path / package.package_path / "dist").glob("*.whl")) + list(
-        (root_path / package.package_path / "dist").glob("*.tar.gz")
-    )
+    assert (package.package_path / "dist").exists(), "Distribution folder should exist"
+    filelist = list((package.package_path / "dist").glob("*.whl")) + list((package.package_path / "dist").glob("*.tar.gz"))
     assert len(filelist) >= 2
 
     package.clean()
-    filelist = list((root_path / package.package_path).rglob("*.py")) + list((root_path / package.package_path).rglob("*.pyi"))
+    filelist = list((package.package_path).rglob("*.py")) + list((package.package_path).rglob("*.pyi"))
     assert len(filelist) == 0
-
-
-def test_package_from_json(tmp_path, pytestconfig):
-    # test data
-    source = pytestconfig.rootpath / "tests/publish/data"
-    root_path = tmp_path
-    publish_path = tmp_path / "publish"
-    publish_path.mkdir(parents=True)
-
-    # TODO: need to endure that the stubs are avaialble in GHA testing
-    stubpacker.ROOT_PATH = tmp_path
-    stubpacker.STUB_PATH = pytestconfig.rootpath
-    # stubpacker.PUBLISH_PATH = publish_path
-    stubpacker.TEMPLATE_PATH = pytestconfig.rootpath / "tests/publish/data/template"
-
-    mpy_version = "v1.18"
-    family = "micropython"
-    pkg_name = "foo-bar-stubs"
-    # todo: include stubs in the test data
-    # note uses `stubs` relative to the project as stub source
-    json = {
-        "name": "foo-bar-stubs",
-        "mpy_version": "1.18",
-        "publish": True,
-        "pkg_version": "1.18.post6",
-        "path": "publish/foo-v1_18-bar-stubs",
-        "stub_sources": [
-            ["Firmware stubs", "stubs/micropython-v1_17-stm32"],
-            ["Frozen stubs", "stubs/micropython-v1_17-frozen/stm32/GENERIC"],
-            ["Core Stubs", "stubs/cpython_core-pycopy"],
-        ],
-        "description": "foo bar stubs",
-        "hash": "b09f9c819c9e98cbd9dfbc8158079146587e2d66",
-    }
-
-    package = stubpacker.StubPackage(pkg_name, version=mpy_version, json_data=json)
-    assert isinstance(package, stubpacker.StubPackage)
-    run_common_package_tests(package, pkg_name, root_path, pkg_type=None)
