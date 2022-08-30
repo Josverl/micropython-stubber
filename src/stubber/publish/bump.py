4f09c19895611902271efec8785f7d20f2b9911e
Jos Verlinde <jos.verlinde@microsoft.com>
2022-08-30 23:31:49 +0200

    add: baseline for pre-release handling

--- src/stubber/publish/bump.py
+++ src/stubber/publish/bump.py
@@ -1,14 +1,20 @@
+from enum import Enum
+
 from packaging.version import Version, parse
 
 
 def bump_postrelease(
     current: Version,
+    rc: int = 0,  # o, to bump post release, or release candidate number
 ) -> Version:
     """
     Increases the post release version number
 
     This allows for a new stub-release to be published while still using the Major.Minor.Patch version numbers of Micropython
-    format: x.y.z.post1, x.y.z.post2 ...
+    if rc = 0(default) : bump post release
+        format: x.y.z.post1, x.y.z.post2 ...
+    if rec specified:
+        drop the post release and set the release candidate number
 
     ref: https://peps.python.org/pep-0440/
     """
@@ -18,14 +24,15 @@ def bump_postrelease(
         parts.append(f"{current.epoch}!")
     # Release segment
     parts.append(".".join(str(x) for x in current.release))
-    # Pre-release
-    if current.pre is not None:
-        parts.append("".join(str(x) for x in current.pre))
-    # BUMP Post-release
-    if current.post is not None:
-        parts.append(f".post{current.post + 1}")
+    if rc != 0:
+        # Pre-release / alpha / beta / rc segment
+        parts.append(f".a{rc}")
     else:
-        parts.append(f".post{1}")
+        # BUMP Post-release
+        if current.post is not None:
+            parts.append(f".post{current.post + 1}")
+        else:
+            parts.append(f".post{1}")
     # Development release
     if current.dev is not None:
         parts.append(f".dev{current.dev}")
--- tests/publish/test_bump.py
+++ tests/publish/test_bump.py
@@ -9,17 +9,39 @@ from stubber.publish.bump import bump_postrelease
         ("1.2.3", "1.2.3.post1"),
         ("1.0.0.post1", "1.0.0.post2"),
         ("1!2.0.0", "1!2.0.0.post1"),
-        ("1.0.0.pre1", "1.0.0.pre1.post1"),
-        ("1.2.3.a", "1.2.3.a0.post1"),  # a--> a0
-        ("1.2.3.a4", "1.2.3.a4.post1"),
-        ("1.2.3.b", "1.2.3.b0.post1"),
-        ("1.2.3.b6", "1.2.3.b6.post1"),
-        ("1.2.3.rc1", "1.2.3.rc1.post1"),
+        ("1.0.0.pre1", "1.0.0.post1"),  # drop prerelease and set postrelease
+        ("1.2.3.a", "1.2.3.post1"),  # drop prerelease and set postrelease
+        ("1.2.3.a4", "1.2.3.post1"),
+        ("1.2.3.b", "1.2.3.post1"),  # drop prerelease and set postrelease
+        ("1.2.3.b6", "1.2.3.post1"),  # drop prerelease and set postrelease
+        ("1.2.3.rc1", "1.2.3.post1"),  # drop prerelease and set postrelease
         ("1.2.3-dev4", "1.2.3.post1-dev4"),  # different order
         ("1.2.3+local.4", "1.2.3.post1+local.4"),  # different order
     ],
 )
-def test_bump(before, after):
+def test_bump_post(before, after):
     version = Version(before)
     result = bump_postrelease(version)
+    assert result == Version(after)
+
+
+@pytest.mark.parametrize(
+    "before, after",
+    [
+        ("1.2.3", "1.2.3.a456"),
+        ("1.0.0.post1", "1.0.0.a456"),
+        ("1!2.0.0", "1!2.0.0.a456"),
+        ("1.0.0.pre1", "1.0.0.a456"),  # drop prerelease and set postrelease
+        ("1.2.3.a", "1.2.3.a456"),  # drop prerelease and set postrelease
+        ("1.2.3.a4", "1.2.3.a456"),
+        ("1.2.3.b", "1.2.3.a456"),  # drop prerelease and set postrelease
+        ("1.2.3.b6", "1.2.3.a456"),  # drop prerelease and set postrelease
+        ("1.2.3.rc1", "1.2.3.a456"),  # drop prerelease and set postrelease
+        ("1.2.3-dev4", "1.2.3.a456-dev4"),  # different order
+        ("1.2.3+local.4", "1.2.3.a456+local.4"),  # different order
+    ],
+)
+def test_bump_pre(before, after):
+    version = Version(before)
+    result = bump_postrelease(version, rc=456)
     assert result == Version(after)
