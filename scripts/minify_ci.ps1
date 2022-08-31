9045b4b38814b85a111f8f80bbb7ce3c33698d74
Jos Verlinde <jos.verlinde@microsoft.com>
2022-08-31 16:55:44 +0200

    chore: remove powershell version of minify

--- scripts/minify_ci.ps1
+++
@@ -1,6 +0,0 @@
-# FIXME: seperate script to work-around poetry install not making the commands executable / findable on CI 
-
-# Minify all versions
-# python -m stubber.stubber minify --all --compile
-# or : 
-poetry run stubber minify --all --compile
