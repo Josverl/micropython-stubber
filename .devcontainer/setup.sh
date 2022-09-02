f66d532cc373c375989f89df7c53c8273e53e170
Jos Verlinde <jos_verlinde@hotmail.com>
2022-09-02 10:55:51 +0000

    dev: use pipx to install  poetry

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # this script is referenced from the codespaces devcontainer dockerfile
 
-
 echo "install poetry before creating the venv" 
-curl -sSL https://install.python-poetry.org | python3 - -y
+# curl -sSL https://install.python-poetry.org | python3 - -y
+pipx install poetry
 
 #poetry will re-use the active venv or create a new one
 echo "start: poetry install "
-/home/vscode/.local/bin/poetry install
+poetry install
 
 echo "Activate virtual environment"
 source .venv/bin/activate
