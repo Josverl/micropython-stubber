bafd7e59dc7b7289a078613a1bd37825ea9f4249
Jos Verlinde <jos_verlinde@hotmail.com>
2022-09-12 16:56:13 +0000

    fix package.bump to allign with pypi/test-pypi

--- tests/publish/test_publish.py
+++ tests/publish/test_publish.py
@@ -2,12 +2,12 @@ from pathlib import Path
 
 import pytest
 from mock import MagicMock
+from packaging.version import Version
 from pytest_mock import MockerFixture
 from stubber.publish.database import get_database
 from stubber.publish.enums import ALL_TYPES, COMBO_STUBS, CORE_STUBS, DOC_STUBS
 from stubber.publish.publish import publish, publish_multiple, publish_one
 from stubber.utils.config import CONFIG, readconfig
-from packaging.version import Version
 
 from .fakeconfig import FakeConfig
 
@@ -47,9 +47,7 @@ def test_publish_one(mocker, tmp_path, pytestconfig, production: bool, dryrun: b
     assert m_build.call_count == build_cnt
     assert m_publish.call_count == publish_cnt
     assert result["result"] in ["Published", "DryRun successful"]
-    assert Version(result["version"]).base_version == Version("1.18").base_version 
-    # TODO: add return and tests
-    # #assert result != None , "Publish failed"
+    assert Version(result["version"]).base_version == Version("1.18").base_version
 
 
 def test_publish_multiple(mocker, tmp_path, pytestconfig):
@@ -68,3 +66,30 @@ def test_publish_multiple(mocker, tmp_path, pytestconfig):
 
     assert m_build.call_count >= 2
     assert m_publish.call_count >= 2
+
+
+def test_publish_prod(mocker, tmp_path, pytestconfig):
+    """Test publish_multiple"""
+
+    production: bool = True
+    dryrun: bool = False
+    # use the test config
+    config = FakeConfig(tmp_path=tmp_path, rootpath=pytestconfig.rootpath)
+    # need to use fake config in two places
+    mocker.patch("stubber.publish.publish.CONFIG", config)
+    mocker.patch("stubber.publish.stubpacker.CONFIG", config)
+
+    m_check: MagicMock = mocker.patch("stubber.publish.publish.StubPackage.check", autospec=True, return_value=True)
+    m_build: MagicMock = mocker.patch("stubber.publish.publish.StubPackage.build", autospec=True, return_value=True)
+    m_pypi: MagicMock = mocker.patch(
+        "stubber.publish.publish.get_pypy_versions", autospec=True, return_value=[Version("1.18.post1"), Version("1.18.post42")]
+    )
+
+    m_publish: MagicMock = mocker.patch("stubber.publish.stubpacker.StubPackage.publish", autospec=True, return_value=True)
+
+    result = publish_one(production=production, dryrun=dryrun, frozen=True, ports="esp32", boards="GENERIC", versions="1.18")
+    assert result
+    assert result["result"] in ["Published", "DryRun successful"]
+    assert Version(result["version"]).base_version == Version("1.18").base_version
+
+    assert Version(result["version"]) == Version("1.18.post43")
