34e8fd6b925c4ab9a283bcae94aa2e7d4702808b
Jos Verlinde <Josverl@users.noreply.github.com>
2022-09-13 23:06:24 +0200

    chore:tweak logging

--- src/stubber/commands/get_docstubs_cmd.py
+++ src/stubber/commands/get_docstubs_cmd.py
@@ -48,8 +48,6 @@ def cli_docstubs(
     Read the Micropython library documentation files and use them to build stubs that can be used for static typechecking.
     """
 
-    log.info(f"stubs_from_docs version {__version__}\n")
-
     if path == CONFIG.repo_path.as_posix():
         # default
         rst_path = CONFIG.mpy_path / "docs" / "library"
