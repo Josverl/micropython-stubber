98874a7847aa594524b3c3879dc2a4166d0eea25
Jos Verlinde <Jos.Verlinde@Microsoft.com>
2022-11-23 15:26:32 +0100

    fix: package not found Exception

--- src/stubber/publish/pypi.py
+++ src/stubber/publish/pypi.py
@@ -1,8 +1,8 @@
 from typing import Optional
 
 from packaging.version import Version, parse
-from pypi_simple import PyPISimple
-
+from pypi_simple import PyPISimple, NoSuchProjectError
+from loguru import logger as log
 
 def get_pypy_versions(package_name: str, base: Optional[Version] = None, production: bool = True):
     """Get all versions of a package from a PyPI endpoint."""
@@ -12,8 +12,12 @@ def get_pypy_versions(package_name: str, base: Optional[Version] = None, product
     else:
         endpoint = "https://test.pypi.org/simple/"
 
-    with PyPISimple(endpoint=endpoint) as client:
-        package_info = client.get_project_page(project=package_name)
+    try:
+        with PyPISimple(endpoint=endpoint) as client:
+            package_info = client.get_project_page(project=package_name)
+    except NoSuchProjectError as e:
+        log.debug(f"Package {package_name} not found on {endpoint}")
+        return []
 
     if not package_info:
         return []
--- tests/publish/test_pypi_versions.py
+++ tests/publish/test_pypi_versions.py
@@ -2,10 +2,6 @@ from packaging.version import Version
 from stubber.publish.pypi import get_pypy_versions
 
 
-def test_get_pypy_versions2():
-    pass
-
-
 def test_get_pypy_versions():
     versions = get_pypy_versions("micropython-esp32-stubs", production=False)
     assert isinstance(versions, list)
