b0f39ad2259ab21ee85a81f9691412c89548c6c1
Braden Mars <bradenmars@bradenmars.me>
2023-01-14 02:13:45 -0600

    fix(codemods): unbind and remove func attrs from newly scoped meths in ScopeableMatcherTransformer
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- src/stubber/codemod/utils.py
+++ src/stubber/codemod/utils.py
@@ -10,17 +10,28 @@ class ScopeableMatcherTransformer(m.MatcherDecoratableTransformer):
 
     scope_matcher: Optional[m.BaseMatcherNode]
 
-    def __init__(self, *, scope_matcher: Optional[m.BaseMatcherNode] = None):
-        self.scope_matcher = scope_matcher
-        if self.scope_matcher is not None:
-            constructed_meths = list(
-                itertools.chain.from_iterable(
-                    [*_gather_constructed_leave_funcs(self).values(), *_gather_constructed_visit_funcs(self).values()]
-                )
-            )
-            scoped_meths = {
-                f.__name__: m.call_if_inside(self.scope_matcher)(getattr(self.__class__, f.__name__)) for f in constructed_meths
-            }
-            # create a dynamically derived class with the transform meth wrapped with scope check.
-            self.__class__ = type(f"{self.__class__.__name__}_{repr(self.scope_matcher)}", (self.__class__,), scoped_meths)
+    def __init__(self):
         super().__init__()
+
+    def _build_scoped_meth(self, method_name: str, scope_matcher: m.BaseMatcherNode):
+        """Build unbound scoped method from parent class."""
+        unbound_meth = getattr(type(self), method_name)
+        unbound_meth.__dict__.pop("_call_if_inside_matcher", None)
+        return m.call_if_inside(scope_matcher)(unbound_meth)
+
+    def with_scope(self, scope_matcher: m.BaseMatcherNode) -> m.MatcherDecoratableTransformer:
+        """Construct a copy of this matcher with visitors scoped to `scope_matcher.`"""
+        constructed_meths = list(
+            itertools.chain.from_iterable(
+                [*_gather_constructed_leave_funcs(self).values(), *_gather_constructed_visit_funcs(self).values()]
+            )
+        )
+        scoped_meths = {f.__name__: self._build_scoped_meth(f.__name__, scope_matcher) for f in constructed_meths}
+        inst_vars = {k: v for k, v in vars(self).items() if k not in {"_matchers", "_extra_leave_funcs", "_extra_visit_funcs"}}
+        # create a dynamically derived class with the transform meth wrapped with scope check.
+        klass = type(
+            f"{self.__class__.__name__}_{repr(scope_matcher)}",
+            (m.MatcherDecoratableTransformer,),
+            {**inst_vars, **scoped_meths, "scope_matcher": scope_matcher},
+        )
+        return klass()
