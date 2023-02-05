e3248491202e628232e676e37f85274ce4401c8e
Jos Verlinde <Jos.Verlinde@Microsoft.com>
2023-02-05 13:53:27 +0100

    refactor switch

--- src/stubber/commands/switch_cmd.py
+++ src/stubber/commands/switch_cmd.py
@@ -1,8 +1,7 @@
 """
-switch 
+switch to a specific version of the micropython repos
 """
 
-import os
 from pathlib import Path
 from typing import Optional, Union
 
@@ -10,7 +9,7 @@ import click
 import stubber.basicgit as git
 from loguru import logger as log
 from stubber.utils.config import CONFIG
-from stubber.utils.repos import match_lib_with_mpy
+from stubber.utils.repos import fetch_repos,  repo_paths
 
 from .cli import stubber_cli
 
@@ -27,14 +26,11 @@ except Exception:
 
 
 @stubber_cli.command(name="switch")
-@click.argument("tag", required=False, type=click.Choice(VERSION_LIST, case_sensitive=False))
+@click.argument("tag",required=False, type=click.Choice(VERSION_LIST, case_sensitive=False))
 @click.option("--path", "-p", default=CONFIG.repo_path.as_posix(), type=click.Path(file_okay=False, dir_okay=True))
 def cli_switch(path: Union[str, Path], tag: Optional[str] = None):
     """
     Switch to a specific version of the micropython repos.
-
-    Specify the version of the MicroPython repo.
-    use of the --tag or --version is deprocated
 
     The Micropython-lib repo will be checked out to a commit that corresponds
     in time to that version tag, in order to allow non-current versions to be
@@ -42,44 +38,11 @@ def cli_switch(path: Union[str, Path], tag: Optional[str] = None):
 
     The repros must be cloned already
     """
-    dest_path = Path(path)
-    if not dest_path.exists():
-        os.mkdir(dest_path)
-    # repos are relative to provided path
-    if dest_path != CONFIG.repo_path:
-        mpy_path = dest_path / "micropython"
-        mpy_lib_path = dest_path / "micropython-lib"
-    else:
-        mpy_path = CONFIG.mpy_path
-        mpy_lib_path = CONFIG.mpy_lib_path
 
-    # if no repos then error
-    if not (mpy_path / ".git").exists():
-        log.error("micropython repo not found")
-        return -1
-    if not (mpy_lib_path / ".git").exists():
-        log.error("micropython-lib repo not found")
-        return -1
-
-    # fetch then switch
-    log.info("fetch updates")
-    git.fetch(mpy_path)
-    git.fetch(mpy_lib_path)
     try:
-        git.fetch(CONFIG.stub_path.parent)
+        mpy_path, mpy_lib_path = repo_paths(Path(path))
     except Exception:
-        log.trace("no stubs repo found : {CONFIG.stub_path.parent}")
-
-    if not tag or tag == "":
-        tag = "latest"
-
-    log.info(f"Switching to {tag}")
-    if tag == "latest":
-        git.switch_branch(repo=mpy_path, branch="master")
-    else:
-        git.checkout_tag(repo=mpy_path, tag=tag)
-    match_lib_with_mpy(version_tag=tag, lib_path=mpy_lib_path)
-
-    log.info(f"{mpy_lib_path} {git.get_tag(mpy_path)}")
-    log.info(f"{mpy_lib_path} {git.get_tag(mpy_lib_path)}")
-    return 0
+        log.error("micropython(-liib) repo not found")
+        return -1
+    result = fetch_repos(tag, mpy_path, mpy_lib_path)
+    return -1 if result else 0
