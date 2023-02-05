33a0ae02021aabd7c46a6afc4cc2ceb4a111fb16
Jos Verlinde <Jos.Verlinde@Microsoft.com>
2023-02-05 13:53:25 +0100

    chore:refactor publish

--- src/stubber/publish/publish.py
+++ src/stubber/publish/publish.py
@@ -28,7 +28,7 @@ NOTE: stubs and publish paths can be located in different locations and reposito
 """
 
 import shutil
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 from loguru import logger as log
 from pysondb import PysonDB
@@ -48,6 +48,10 @@ from stubber.utils.versions import clean_version
 # micropython-doc-stubs
 # ######################################
 
+from typing import NewType
+
+Status = NewType('Status', Dict[str, Union[ str, None]])
+
 def publish(
     *,
     db: PysonDB,
@@ -60,47 +64,136 @@ def publish(
     clean: bool = False,  # clean up afterwards
     port: str = "",
     board: str = "",
-) -> Dict[str, Any]:    # sourcery skip: default-mutable-arg, require-parameter-annotation
+) -> Status:    # sourcery skip: default-mutable-arg, require-parameter-annotation
     """
     Publish a package to PyPi
     look up the previous package version in the dabase, and only publish if there are changes to the package
     - change determied by hash across all files
 
+    Build
+        - update package files
+        - build the wheels and sdist
+    Publish 
+        - publish to PyPi
+        - update database with new hash    
     """
-    # semver, no prefix
     version = clean_version(version, drop_v=True, flat=False)
     # package name for firmware package
     pkg_name = package_name(pkg_type=pkg_type, port=port, board=board, family=family)
-    status: Dict[str, Any] = {"result": "-", "name": pkg_name, "version": version, "error": None}
+    status: Status = Status({"result": "-", "name": pkg_name, "version": version, "error": None})
     log.debug("=" * 40)
+    # #####################################################
+    # Build the package
+    package, ok, status = update_package(db, pkg_type, version, family, port, board, pkg_name, status)
+    if not ok:
+        return failed_build(pkg_name, status)
+    # If there are changes to the package, then publish it
+    if package.is_changed():
+        build_ok = build_dist(production, force, pkg_name, status, package)
+        if not build_ok:
+            return failed_build(pkg_name, status)
 
-    if package_info := get_package_info(
-        db,
-        CONFIG.publish_path,
-        pkg_name=pkg_name,
-        mpy_version=version,
-        ):
-        # create package from the information retrieved from the database
-        package = StubPackage(pkg_name, version=version, json_data=package_info)
+    # #####################################################
+    # Publish the package
+    if dryrun:  # pragma: no cover
+        log.warning("Dryrun: Updated package is NOT published.")
+        status["result"] = "DryRun successful"
+        if clean:
+            package.clean()
+        return status
+    
+    if package.is_changed() or force:
+        if force:
+            log.warning("Force: Update of package")
+            old_ver = package.pkg_version
+            if package.mpy_version == "latest":
+                new_ver = prerelease_package_version(package, production)
+                package.pkg_version = new_ver
+            else:
+                new_ver = next_package_version(package, production)
+            # to get the next version
+            log.debug(f"{pkg_name}: bump version for {old_ver} to {new_ver} {production}")
+            # Update hashes
+            package.update_hashes()
+            package.write_package_json()
 
+            status["version"] = package.pkg_version
+
+        if package.mpy_version == "latest":
+            log.warning("version: `latest` package will only be available on Github, and not published to PyPi.")
+            status["result"] = "Published to GitHub"
+        else:
+            build_ok = package.publish(production=production)
+            if not build_ok:
+                return failed_publish(pkg_name, package, status)
+            status["result"] = "Published to PyPi" if production else "Published to Test-PyPi"
+            db.add(package.to_dict())
+            db.commit()
     else:
-        log.debug(f"No package found for {pkg_name} in database, creating new package")
-        package = create_package(
-            pkg_name,
-            mpy_version=version,
-            port=port,
-            board=board,
-            family=family,
-            pkg_type=pkg_type,
-        )
+        log.debug(f"No changes to package : {package.package_name} {package.pkg_version}")
+    if clean:
+        package.clean()
+    return status
+
+def build_dist(production:bool, force:bool, pkg_name:str, status:Status, package: StubPackage)-> bool:
+    if not force:  # pragma: no cover
+        log.info(f"Found changes to package sources: {package.package_name} {package.pkg_version}")
+        log.debug(f"Old hash {package.hash} != New hash {package.create_hash()}")
+        # if not dryrun:
+        # only bump version if we are going to publish
+        # get last published version.postXXX from PyPI and update version if different
+        # try to get version from PyPi and increase past that
+    old_ver = package.pkg_version
+    status["version"] = package.pkg_version = updated_package_version(production, package)
+        # to get the next version
+    log.debug(f"{pkg_name}: bump version for {old_ver} to {package.pkg_version } {production}")
+    # Update hashes
+    package.update_hashes()
+    package.write_package_json()
+    log.trace(f"New hash: {package.package_name} {package.pkg_version} {package.hash}")
+    return package.build()
+
+def update_package(db:PysonDB, pkg_type, version, family:str, port:str, board:str, pkg_name:str, status:Status) -> Tuple[StubPackage, bool, Status]:
+    """Update the package files and check that the sources are available"""
+    package = get_package(db, pkg_type, version, family, port, board, pkg_name)
     log.info(f"Processing {package.package_path.name}")
     log.trace(f"{package.package_path.as_posix()}")
     status["version"] = package.pkg_version
     # check if the sources exist
+    ok = package_sources_available(pkg_name, status, package)
+    if not ok:
+        log.warning(f"{pkg_name}: skipping as one or more source stub folders are missing")
+        shutil.rmtree(package.package_path.as_posix())
+        package._publish = False  # type: ignore
+        return package, ok, status
+    try:
+        package.update_package_files()
+        package.update_included_stubs()
+        package.check()
+    except Exception as e:  # pragma: no cover
+        log.error(f"{pkg_name}: {e}")
+        status["error"] = str(e)
+        ok = False
+        return package, ok, status
+    return package, ok, status
+
+def updated_package_version(production, package) -> str:
+    """Get the next version for the package"""
+    return (
+        prerelease_package_version(package, production)
+        if package.mpy_version == "latest"
+        else next_package_version(package, production)
+    )
+
+def package_sources_available(pkg_name, status, package) -> bool:
+    """
+    Check if (all) the packages sources exist.
+    """
     ok = True
     for (name, path) in package.stub_sources:
         if not (CONFIG.stub_path / path).exists():
-            msg = f"{pkg_name}: source '{name}' not found: {CONFIG.stub_path / path}"
+            # todo: below is a workaround for different types, but where is the source of this difference coming from?
+            msg = f"{pkg_name}: source '{name._value_}' not found: {CONFIG.stub_path / path}" if isinstance(name, StubSource) else f"{pkg_name}: source '{name}' not found: {CONFIG.stub_path / path}"
             if name != StubSource.FROZEN:
                 log.warning(msg)
                 status["error"] = msg
@@ -108,120 +201,48 @@ def publish(
             else:
                 # not a blocking issue if there are no frozen stubs, perhaps this port/board does not have any
                 log.warning(msg)
-    if not ok:
-        log.warning(f"{pkg_name}: skipping as one or more source stub folders are missing")
-        shutil.rmtree(package.package_path.as_posix())
+    return ok
 
-        package._publish = False
-        # TODO Save ?
-        return status
-    try:
+def get_package(db, pkg_type, version, family, port, board, pkg_name) -> StubPackage:
+    """Get the package from the database or create a new one if it does not exist."""
+    if package_info := get_package_info(
+        db,
+        CONFIG.publish_path,
+        pkg_name=pkg_name,
+        mpy_version=version,
+        ):
+        # create package from the information retrieved from the database
+        return StubPackage(pkg_name, version=version, json_data=package_info)
 
-        package.update_package_files()
-        package.update_included_stubs()
-        package.check()
-    except Exception as e:  # pragma: no cover
-        log.error(f"{pkg_name}: {e}")
-        status["error"] = str(e)
-        return status
-
-    # If there are changes to the package, then publish it
-    if package.is_changed():
-        if not force:  # pragma: no cover
-            log.info(f"Found changes to package : {package.package_name} {package.pkg_version}")
-            log.debug(f"Old hash {package.hash} != New hash {package.create_hash()}")
-        # if not dryrun:
-        # only bump version if we are going to publish
-        # get last published version.postXXX from PyPI and update version if different
-        # try to get version from PyPi and increase past that
-        old_ver = package.pkg_version
-        if package.mpy_version == "latest":
-            new_ver = str(bump_postrelease(Version("1.20"), rc=744))  # FIXME: hardcoded version
-            package.pkg_version = new_ver
-        else:
-            new_ver = next_package_version(package, production)
-        # to get the next version
-        log.debug(f"{pkg_name}: bump version for {old_ver} to {new_ver} {production}")
-
-        # Update hashes
-        package.update_hashes()
-        package.write_package_json()
-
-        status["version"] = package.pkg_version
-
-        log.trace(f"New hash: {package.package_name} {package.pkg_version} {package.hash}")
-        result = package.build()
-        if not result:
-            return failed_build(pkg_name, status)
-        if dryrun:  # pragma: no cover
-            log.warning("Dryrun: Updated package is NOT published.")
-            status["result"] = "DryRun successful"
-        elif package.mpy_version == "latest":
-            log.warning("version: `latest` package will only be avaialbe on GIT, and not  published to PyPi.")
-        else:
-            result = package.publish(production=production)
-            if not result:
-                return failed_publish(pkg_name, package, status)
-            status["result"] = "Published"
-            db.add(package.to_dict())
-            db.commit()
-
-    elif force:
-        # if not dryrun:
-        # only bump version if we are going to publish
-        # get last published version.postXXX from PyPI and update version if different
-        # try to get version from PyPi and increase past that
-        old_ver = package.pkg_version
-        if package.mpy_version == "latest":
-            new_ver = str(bump_postrelease(Version("1.20"), rc=744))  # FIXME: hardcoded version
-            package.pkg_version = new_ver
-        else:
-            new_ver = next_package_version(package, production)
-        # to get the next version
-        log.debug(f"{pkg_name}: bump version for {old_ver} to {new_ver} {production}")
-
-        # Update hashes
-        package.update_hashes()
-        package.write_package_json()
-
-        status["version"] = package.pkg_version
-
-        log.trace(f"New hash: {package.package_name} {package.pkg_version} {package.hash}")
-        result = package.build()
-        if not result:
-            return failed_build(pkg_name, status)
-        if dryrun:  # pragma: no cover
-            log.warning("Dryrun: Updated package is NOT published.")
-            status["result"] = "DryRun successful"
-        elif package.mpy_version == "latest":
-            log.warning("version: `latest` package will only be avaialbe on GIT, and not  published to PyPi.")
-        else:
-            result = package.publish(production=production)
-            if not result:
-                return failed_publish(pkg_name, package, status)
-            status["result"] = "Published"
-            db.add(package.to_dict())
-            db.commit()
-
-    else:
-        log.debug(f"No changes to package : {package.package_name} {package.pkg_version}")
-    if clean:
-        package.clean()
-    return status
+    log.debug(f"No package found for {pkg_name} in database, creating new package")
+    return create_package(
+        pkg_name,
+        mpy_version=version,
+        port=port,
+        board=board,
+        family=family,
+        pkg_type=pkg_type,
+    )
 
 
-def failed_publish(pkg_name:str, package, status:dict) -> dict:
+def failed_publish(pkg_name:str, package, status:Status) -> Status:
     """Publish failed, so return the status."""
     log.warning(f"{pkg_name}: Publish failed for {package.pkg_version}")
     status["error"] = "Publish failed"
     return status
 
 
-def failed_build(pkg_name:str, status:dict) -> dict:
+def failed_build(pkg_name:str, status:Status) -> Status:
     """Build failed, so return the status."""
     log.warning(f"{pkg_name}: skipping as build failed")
     status["error"] = "Build failed"
     return status
+
+def prerelease_package_version(package:StubPackage, prod:bool=False)->str:
+    """Get the next prerelease version for the package."""
+    base = Version("1.20") # TODO hardcoded version - should be the next minor version after the last release
+    rc = 744  # FIXME: #307 hardcoded prerelease version - should be based on the git commit count
+    return str(bump_postrelease(base, rc=rc))  
 
 def next_package_version(package:StubPackage, prod:bool=False)->str:
     """Get the next version for the package."""
--- tests/publish/test_publish.py
+++ tests/publish/test_publish.py
@@ -1,3 +1,5 @@
+"""Test publish module"""
+from pathlib import Path
 import pytest
 from mock import MagicMock
 from packaging.version import Version
@@ -7,32 +9,33 @@ from stubber.publish.publish import publish_multiple, publish_one
 
 from .fakeconfig import FakeConfig
 
-# @pytest.mark.parametrize(
-#     "pkg_type, ports, boards, versions",
-#     [
-#         (COMBO_STUBS, ["esp32"], ["GENERIC"], ["1.18"]),
-#         (DOC_STUBS, [], [], ["1.18"]),
-#     ],
-# )
-
+ALL_PUBLISH_RESULTS = {"Published to PyPi","Published to Test-PyPi", "DryRun successful", "-"}
 
 @pytest.mark.parametrize(
-    "production, dryrun, check_cnt, build_cnt, publish_cnt",
+    "production, dryrun, is_changed, check_cnt, build_cnt, publish_cnt",
     [
-        (True, True, 1, 1, 0),
-        (True, False, 1, 1, 1),
-        (False, True, 1, 1, 0),
-        (False, False, 1, 1, 1),
+        # not changed
+        (True, True, False, 1, 0, 0),
+        (True, False, False, 1, 0, 0),
+        (False, True, False, 1, 0, 0),
+        (False, False, False, 1, 0, 0),
+        # dryrun
+        (True, True, True, 1, 1, 0),
+        (False, True, True, 1, 1, 0),
+        # changed
+        (True, False, True, 1, 1, 1),
+        (False, False, True, 1, 1, 1),
     ],
 )
 @pytest.mark.mocked
 @pytest.mark.integration
 def test_publish_one(
     mocker: MockerFixture,
-    tmp_path,
-    pytestconfig,
+    tmp_path: Path,
+    pytestconfig: pytest.Config,
     production: bool,
     dryrun: bool,
+    is_changed: bool,
     check_cnt: int,
     build_cnt: int,
     publish_cnt: int,
@@ -51,6 +54,7 @@ def test_publish_one(
     mocker.patch("stubber.publish.stubpacker.CONFIG", config)
 
     m_check: MagicMock = mocker.patch("stubber.publish.publish.StubPackage.check", autospec=True, return_value=True)
+    m_is_changed: MagicMock = mocker.patch("stubber.publish.publish.StubPackage.is_changed", autospec=True, return_value=is_changed)
     m_build: MagicMock = mocker.patch("stubber.publish.publish.StubPackage.build", autospec=True, return_value=True)
     m_publish: MagicMock = mocker.patch("stubber.publish.stubpacker.StubPackage.publish", autospec=True, return_value=True)
 
@@ -59,13 +63,13 @@ def test_publish_one(
     assert m_check.call_count == check_cnt
     assert m_build.call_count == build_cnt
     assert m_publish.call_count == publish_cnt
-    assert result["result"] in ["Published", "DryRun successful"]
+    assert result["result"] in ALL_PUBLISH_RESULTS
     assert Version(result["version"]).base_version == Version("1.18").base_version
 
 
 @pytest.mark.mocked
 @pytest.mark.integration
-def test_publish_multiple(mocker, tmp_path, pytestconfig):
+def test_publish_multiple(mocker: MockerFixture, tmp_path: Path, pytestconfig : pytest.Config):
     """Test publish_multiple"""
     # test requires that the stubs are cloned locally
     if not (pytestconfig.rootpath / "repos/micropython-stubs").exists():
@@ -75,24 +79,48 @@ def test_publish_multiple(mocker, tmp_path, pytestconfig):
     config = FakeConfig(tmp_path=tmp_path, rootpath=pytestconfig.rootpath)
     mocker.patch("stubber.publish.publish.CONFIG", config)
     mocker.patch("stubber.publish.stubpacker.CONFIG", config)
+    m_is_changed: MagicMock = mocker.patch("stubber.publish.publish.StubPackage.is_changed", autospec=True, return_value=False)
 
     m_check: MagicMock = mocker.patch("stubber.publish.publish.StubPackage.check", autospec=True, return_value=True)
     m_build: MagicMock = mocker.patch("stubber.publish.publish.StubPackage.build", autospec=True, return_value=True)
     m_publish: MagicMock = mocker.patch("stubber.publish.publish.StubPackage.publish", autospec=True, return_value=True)
 
+    # Test dryrun - not changed
     result = publish_multiple(production=False, frozen=True, dryrun=True)
     assert len(result) > 0
+    assert m_build.call_count == 0
+    # assert m_check.call_count == 1
+    assert m_publish.call_count == 0
+
+    # Test dryrun -  changed
+    m_build.reset_mock()
+    m_check.reset_mock()
+    m_publish.reset_mock()
+    m_is_changed.reset_mock()
+    m_is_changed.return_value = True    
+    result = publish_multiple(production=False, frozen=True, dryrun=True)
+    assert len(result) > 0
+    assert m_build.call_count >= 1
+    # assert m_check.call_count == 1
+    assert m_publish.call_count == 0
+
+    # Test Actual + changed
+    m_build.reset_mock()
+    m_check.reset_mock()
+    m_publish.reset_mock()
+    m_is_changed.reset_mock()
+    m_is_changed.return_value = True
+
     result = publish_multiple(production=False, frozen=True, dryrun=False)
     assert len(result) > 0
-
-    assert m_build.call_count >= 2
-    assert m_publish.call_count >= 2
-    assert m_check.call_count >= 2
+    assert m_build.call_count >= 1
+    # assert m_check.call_count == 1
+    assert m_publish.call_count >= 1
 
 
 @pytest.mark.integration
 @pytest.mark.mocked
-def test_publish_prod(mocker, tmp_path, pytestconfig):
+def test_publish_prod(mocker: MockerFixture, tmp_path: Path, pytestconfig : pytest.Config):
     """Test publish_multiple"""
     if not (pytestconfig.rootpath / "repos/micropython-stubs").exists():
         pytest.skip("Integration test: micropython-stubs repo not found")
@@ -112,14 +140,15 @@ def test_publish_prod(mocker, tmp_path, pytestconfig):
     )
 
     m_publish: MagicMock = mocker.patch("stubber.publish.stubpacker.StubPackage.publish", autospec=True, return_value=True)
-
+    m_is_changed: MagicMock = mocker.patch("stubber.publish.publish.StubPackage.is_changed", autospec=True, return_value=True)
     result = publish_one(production=production, dryrun=dryrun, frozen=True, ports="esp32", boards="GENERIC", versions="1.18")
+
     assert result
-    assert result["result"] in ["Published", "DryRun successful"]
+    assert result["result"] in ALL_PUBLISH_RESULTS
     assert Version(result["version"]).base_version == Version("1.18").base_version
 
     assert Version(result["version"]) == Version("1.18.post43")
     assert m_check.call_count == 1
     assert m_build.call_count == 1
-    assert m_publish.call_count == 1
     assert m_pypi.call_count == 1
+    assert m_publish.call_count == 1
