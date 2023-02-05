2193a03f7de95a50b96e8fb450d2dc5fc9aecada
Jos Verlinde <Jos.Verlinde@Microsoft.com>
2023-02-05 13:55:11 +0100

    chore: refactor rst fixes - p1

--- src/stubber/rst/lookup.py
+++ src/stubber/rst/lookup.py
@@ -20,6 +20,21 @@ __all__ = [
 
 
 # modules documented with base name only
+from dataclasses import dataclass
+from typing import List, Optional, Tuple
+
+@dataclass
+class Fix:
+    from_: str
+    "the thing that should be fixed"
+    to: str
+    "the improved version"
+    module: Optional[str] = None
+    "Only valid for the named module"
+    is_re: bool = False
+    "is a regex"
+
+
 U_MODULES = [
     "os",
     "time",
@@ -33,13 +48,16 @@ U_MODULES = [
     "struct",
     "zlib",
 ]
-
+"""
+List of modules that are documented with the base name only, 
+but can also be imported with a `u` prefix
+"""
 
 # Some classes are documented as functions
 # This table is used to try to correct the errors in the documentation.
 # it is applied to each .rst file after loading the contents.
 
-RST_DOC_FIXES = [
+RST_DOC_FIXES :List[Tuple[str,str]] = [
     # ------------------------------------------------------------------------------------------------
     # re.rst - function and class with the same name
     # todo: issue https://github.com/micropython/micropython/issues/8273
@@ -58,6 +76,7 @@ RST_DOC_FIXES = [
     # uselect.rst
     (".. function:: poll(", ".. class:: poll("),
 ]
+
 
 # docstubs generation, exclude stub generation for below stubs.
 DOCSTUB_SKIP = [
@@ -170,35 +189,38 @@ MODULE_GLUE = {
 #   ( "from", "to"),
 #   ( "from", "to", "optional function, Class or Class.Method"),
 # ]
+
+
+
 PARAM_FIXES = [
-    ("\\*", "*"),  # change weirdly written wildcards \* --> *
-    (r"\**", "*"),  # change weirdly written wildcards \* --> *
-    (r"/*", "*"),  # change weirdly written wildcards \* --> *
+    Fix("\\*", "*"),  # change weirdly written wildcards \* --> *
+    Fix(r"\**", "*"),  # change weirdly written wildcards \* --> *
+    Fix(r"/*", "*"),  # change weirdly written wildcards \* --> *
     # ("**", "*"),  # change weirdly written wildcards \* --> *
-    ("'param'", "param"),  # loose notation in documentation
+    Fix("'param'", "param"),  # loose notation in documentation
     # illegal keywords
-    (
+    Fix(
         "lambda",
         "lambda_fn",
     ),
     # method:: ADC.read_timed_multi((adcx, adcy, ...), (bufx, bufy, ...), timer)
-    (
+    Fix(
         "(adcx, adcy, ...), (bufx, bufy, ...)",
         "adcs, bufs",
     ),
     # network.AbstractNIC
     # ifconfig([(ip, subnet, gateway, dns)])
-    (
+    Fix(
         "(ip, subnet, gateway, dns)",
         "configtuple",
     ),
     # pyb.hid((buttons, x, y, z))
-    (
+    Fix(
         "(buttons, x, y, z)",
         "hidtuple:Tuple",
     ),
     # esp v1.15.2 .. function:: getaddrinfo((hostname, port, lambda))
-    (
+    Fix(
         "(hostname, port, lambda)",
         "tuple[str,int,Callable]",
     ),
@@ -208,122 +230,130 @@ PARAM_FIXES = [
     #     "cert_reqs=CERT_NONE",
     #     "cert_reqs=None",
     # ),
-    # network
-    # WLANWiPy.ifconfig(if_id=0, config=['dhcp' or configtuple])
-    (
-        "='dhcp' or configtuple: Optional[Any]=None",
-        ": Union[str,Tuple]='dhcp'",
+    # # network
+    # # WLANWiPy.ifconfig(if_id=0, config=['dhcp' or configtuple])
+    # Fix(
+    #     "config=['dhcp' or configtuple]",
+    #     "config: Union[str,Tuple]='dhcp'"
+    # ),
+    Fix(
+        "config='dhcp' or configtuple: Optional[Any]=None",
+        "config: Union[str,Tuple]='dhcp'"
     ),
+    # (
+    #     "='dhcp' or configtuple: Optional[Any]=None",
+    #     ": Union[str,Tuple]='dhcp'",
+    # ),
     # network
     # CC3K.patch_program('pgm')
-    (
+    Fix(
         "'pgm')",
         "cmd:str ,/)",
     ),
     # network
-    (
+    Fix(
         "block_device or path",
         "block_device_or_path",
     ),  #
     # network
     # ifconfig
-    (
+    Fix(
         "(ip, subnet, gateway, dns):Optional[Any]=None",
         "configtuple: Optional[Tuple]",
     ),
     # framebuffer
     # unresolvable parameter defaults # FrameBuffer: def __init__
-    (
+    Fix(
         "stride=width",
         "stride=-1",
     ),
     # machine.Pin.__init__ constructor - Defaults assumed from the documentation.
     # fixed in doc v1.18+
-    (
+    Fix(
         ", value, drive, alt",
         ", value=None, drive=0, alt=-1",
     ),
     # machine.Pin.irq ...
-    (
+    Fix(
         "trigger=(IRQ_FALLING | IRQ_RISING)",
         "trigger=IRQ_FALLING ",
     ),
     ## fixes for machine.py class constants
     # # BUG: This is not OK
-    (
+    Fix(
         "pins=(SCK, MOSI, MISO)",
         "pins:Optional[Tuple]",
     ),  #
     ## rp2.PIO.irq
-    (
+    Fix(
         "trigger=IRQ_SM0|IRQ_SM1|IRQ_SM2|IRQ_SM3",
         "trigger=IRQ_SM0",
     ),
     # SPI.INIT - to fix error: Non-default argument follows default argument
     # ✅ fixed in doc v1.18+
-    (
+    Fix(
         "prescaler, polarity=1",
         "prescaler=1, polarity=1",
     ),
     # network.LAN.init
     # def __init__(self, id, *, phy_type=<board_default>, phy_addr=<board_default>, phy_clock=<board_default>) -> None:
-    ("=<board_default>", "=0"),
+    Fix("=<board_default>", "=0"),
     # ssl
     # def wrap_socket(sock, server_side=False, keyfile=None, certfile=None, cert_reqs=CERT_NONE, ca_certs=None, do_handshake=True) -> Any:
-    ("cert_reqs=CERT_NONE", "cert_reqs=None"),
+    Fix("cert_reqs=CERT_NONE", "cert_reqs=None"),
     # struct.pack & pack_into
     # def pack(fmt, v1, v2, *args) -> bytes:
-    (", v1, v2,", ", v1,"),
+    Fix(", v1, v2,", ", v1,"),
     # esp32.RMT
     #     # def write_pulses(self, duration, data=True) -> Any:
     #     def write_pulses(self, duration, data:Union[bool,int]=True) -> Any:
-    ("duration, data=True", "duration, data:Union[bool,int]=True"),
+    Fix("duration, data=True", "duration, data:Union[bool,int]=True"),
     # --------------------------------------------------------------------
     # machine
     # machine.PWM
     #     # def __init__(self, dest, *, freq, duty_u16, duty_ns) -> None: ...
     #     def __init__(self, dest, *, freq=0,duty=0, duty_u16=0, duty_ns=0) -> None: ...
-    ("dest, *, freq, duty_u16, duty_ns", "dest, *, freq=0,duty=0, duty_u16=0, duty_ns=0"),
+    Fix("dest, *, freq, duty_u16, duty_ns", "dest, *, freq=0,duty=0, duty_u16=0, duty_ns=0"),
     # machine.ADC
     #     # def __init__(self, id, *, sample_ns, atten) -> None: ...
     #     def __init__(self, id, *, sample_ns:Optional[int]=0, atten:Optional[int]=ATTN_0DB) -> None: ...
-    ("id, *, sample_ns, atten", "id, *, sample_ns:Optional[int]=0, atten:Optional[int]=ATTN_0DB"),
+    Fix("id, *, sample_ns, atten", "id, *, sample_ns:Optional[int]=0, atten:Optional[int]=ATTN_0DB"),
     # machine.I2C
     #     # def __init__(self, id, *, scl, sda, freq=400000) -> None: ...
     #     def __init__(self, id=-1, *, scl:Optional[Pin]=None, sda:Optional[Pin]=None, freq=400000) -> None: ...
-    (
+    Fix(
         "id, *, scl, sda, freq=400000",
         "id:Union[int,str]=-1, *, scl:Optional[Union[Pin,str]]=None, sda:Optional[Union[Pin,str]]=None, freq=400_000",
     ),
     # network.WLAN
     # def config(self, param) -> Any:
     # def config(self, *args, **kwargs) -> Any:
-    ("param", "*args, **kwargs", "WLAN.config"),
+    Fix("param", "*args, **kwargs", "WLAN.config"),
     # machine.UART
     #     def __init__(self, id, ...) -> None: ...
     #     def __init__(self, id, *args, **kwargs) -> None: ...
-    ("id, ...", "id, *args, **kwargs", "UART.__init__"),
+    Fix("id, ...", "id, *args, **kwargs", "UART.__init__"),
     # machine.SPI
     #     #    def __init__(self, id, *args) -> None: ...
     #     def __init__(self, id, *args, **kwargs) -> None: ...
-    ("id, ...", "id, *args, **kwargs", "SPI.__init__"),
+    Fix("id, ...", "id, *args, **kwargs", "SPI.__init__"),
     # machine.Signal
     # def __init__(self, pin_obj, invert=False) -> None: ...
     # def __init__(self, pin_obj, *args, invert=False) -> None: ...
-    ("pin_obj, invert", "pin_obj, *args, invert", "Signal.__init__"),
+    Fix("pin_obj, invert", "pin_obj, *args, invert", "Signal.__init__"),
     # machine.Timer
     # def __init__(self, id, /, *args) -> None: ...
     # def init(self,id, *, mode=PERIODIC, period=-1, callback=None) -> None: ...
-    ("id, /, ...", "id=-1, *args, **kwargs", "Timer.__init__"),
+    Fix("id, /, ...", "id=-1, *args, **kwargs", "Timer.__init__"),
     # --------------------------------------------------------------------
     # pyb
     # def freq(sysclk, hclk, pclk1, pclk2) -> Tuple:
     # def freq(sysclk=0, hclk=0, pclk1=0, pclk2=0) -> Tuple:
-    ("sysclk, hclk, pclk1, pclk2", "sysclk=0, hclk=0, pclk1=0, pclk2=0"),
+    Fix("sysclk, hclk, pclk1, pclk2", "sysclk=0, hclk=0, pclk1=0, pclk2=0"),
     # Timer.__init__
     # def __init__(self, id, *args) -> None: ...
     # def __init__(self, id, *, freq=..., prescaler=..., period=..., mode=UP, div=1, callback=None, deadtime=0) -> None:
-    (
+    Fix(
         "id, *args",
         "id, *, freq=-1, prescaler=-1, period=-1, mode=UP, div=1, callback=None, deadtime=0",
         "Timer.__init__",
@@ -331,21 +361,23 @@ PARAM_FIXES = [
     # Timer.channel
     # def channel(self, channel, mode, *args) -> Any:
     # def channel(self, channel, mode, pin=None, *args) -> Any:
-    ("channel, mode, ...", "channel, mode, pin=None, *args"),
+    Fix("channel, mode, ...", "channel, mode, pin=None, *args"),
     # pyb SPI
     # def __init__(self, bus, *args) -> None: ...
     # def __init__(self,bus,  mode, baudrate=328125, *, prescaler=-1, polarity=1, phase=0, bits=8, firstbit=MSB, ti=False, crc=None) -> None:
-    (
+    Fix(
         "bus, ...",
         "bus,  mode, baudrate=328125, *, prescaler=-1, polarity=1, phase=0, bits=8, firstbit=MSB, ti=False, crc=None",
     ),
     # PYB CAN.setfiler
     # def setfilter(self, bank, mode, fifo, params, *, rtr, extframe=False) -> None:
     # def setfilter(self, bank, mode, fifo, params, *, rtr=..., extframe=False) -> None:
-    (
+    Fix(
         "bank, mode, fifo, params, *, rtr, extframe=False",
         "bank, mode, fifo, params, *, rtr=None, extframe=False",
     ),
+    # DOC: DocUpdate ? deal with overloads for Flash and Partition .readblock/writeblocks
+    Fix("block_num, buf, offset", "block_num, buf, offset: Optional[int] = 0")
 ]
 
 # List of classes and their parent classes that should be added to the class definition
