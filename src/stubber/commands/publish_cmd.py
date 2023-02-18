d1cd2ef94ed064e934c8ee8de5ddada7afec5946
Jos Verlinde <Jos.Verlinde@Microsoft.com>
2023-02-18 23:04:57 +0100

    fix:board allcaps

--- src/stubber/commands/publish_cmd.py
+++ src/stubber/commands/publish_cmd.py
@@ -3,7 +3,7 @@ from typing import List, Union
 import click
 from loguru import logger as log
 from stubber.commands.cli import stubber_cli
-from stubber.publish.package import GENERIC_L
+from stubber.publish.package import GENERIC_U
 from stubber.publish.publish import publish_multiple
 from tabulate import tabulate
 from stubber.utils.config import CONFIG
@@ -35,7 +35,7 @@ from stubber.utils.config import CONFIG
     "-b",
     "boards",
     multiple=True,
-    default=[GENERIC_L],  # or "auto" ?
+    default=[GENERIC_U],  # or "auto" ?
     show_default=True,
     help="multiple: ",
 )
