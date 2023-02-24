f44c6ccf9427b0515dc73ed35cbb968d91d8d43f
Jos Verlinde <Jos.Verlinde@Microsoft.com>
2023-02-24 23:38:40 +0100

    always use pipx to run mpycross-removes dependency on mpy-cross

--- src/stubber/variants.py
+++ src/stubber/variants.py
@@ -4,7 +4,7 @@ POC to create a variants of createstubs.py
 
 import subprocess
 import tempfile
-from typing import Union
+from typing import List, Union
 import libcst as cst
 import libcst.codemod as codemod
 from pathlib import Path
@@ -16,6 +16,8 @@ from loguru import logger as log
 
 from stubber.utils.post import run_black
 from stubber.minify import minify, cross_compile
+
+ALL_VARIANTS = [CreateStubsVariant.BASE, CreateStubsVariant.MEM, CreateStubsVariant.DB, CreateStubsVariant.LVGL]
 
 
 def cross_compile(
@@ -33,10 +35,7 @@ def cross_compile(
         temp_file = Path(temp_file)
         temp_file.write_text(source)
         source_file = temp_file
-    if version:
-        cmd = ["pipx", "run", f"mpy-cross=={version}"]
-    else:
-        cmd = ["mpy-cross"]
+    cmd = ["pipx", "run", f"mpy-cross=={version}"] if version else ["pipx", "run", "mpy-cross"]
     # Add params
     cmd += ["-O2", str(source_file), "-o", str(target), "-s", "createstubs.py"]
     log.trace(" ".join(cmd))
@@ -49,59 +48,56 @@ def cross_compile(
     return result.returncode
 
 
-# read base createstubs.py
-base_path = Path.cwd() / "src" / "stubber" / "board"
+def create_variants(base_path: Path, make_variants: List[CreateStubsVariant] = ALL_VARIANTS, version: str = ""):
+    """Create variants of createstubs.py"""
+    ctx = codemod.CodemodContext()
+    base_file = base_path / "createstubs.py"
+    log.info(f"Reading : {base_file}")
+    base_txt = (base_path / "createstubs.py").read_text()
+    base_module = cst.parse_module(base_txt)
 
+    for var in make_variants:
+        # Transform base to createstubs.py variant
 
-ctx = codemod.CodemodContext()
-base_file = base_path / "createstubs.py"
-log.info(f"Reading : {base_file}")
-base_txt = (base_path / "createstubs.py").read_text()
-base_module = cst.parse_module(base_txt)
+        suffix = "" if var == CreateStubsVariant.BASE else f"_{var.value}"
 
+        variant_path = base_path / f"createstubs{suffix}.py"
+        minified_path = base_path / f"createstubs{suffix}_min.py"
+        mpy_path = base_path / f"createstubs{suffix}_mpy.mpy"  # intentional
 
-for var in [CreateStubsVariant.BASE, CreateStubsVariant.MEM, CreateStubsVariant.DB, CreateStubsVariant.LVGL]:
-    # Transform base to createstubs.py variant
+        if var != CreateStubsVariant.BASE:
+            # No need to create base variant as it is the same as the base file
+            log.info(f"Transforming to {var.value} variant")
+            cm = CreateStubsCodemod(ctx, variant=var)
+            variant = cm.transform_module(base_module)
 
-    suffix = "" if var == CreateStubsVariant.BASE else f"_{var.value}"
+            # write low_mem_variant.code to file
+            log.info(f"Write variant {var.value} to {variant_path}")
+            with open(variant_path, "w") as f:
+                f.write(variant.code)
 
-    variant_path = base_path / f"createstubs{suffix}.py"
-    minified_path = base_path / f"createstubs{suffix}_min.py"
-    mpy_path = base_path / f"createstubs{suffix}_mpy.mpy"  # intentional
+            # format file with black
+            run_black(variant_path, capture_output=True)
+            # TODO: check with pyright if it is valid python
 
-    if var != CreateStubsVariant.BASE:
-        # No need to create base variant as it is the same as the base file
-        log.info(f"Transforming to {var.value} variant")
-        cm = CreateStubsCodemod(ctx, variant=var)
-        variant = cm.transform_module(base_module)
+        # Minify file with pyminifier
+        log.info(f"\nMinifying to {minified_path.name}")
+        minify(variant_path, minified_path, keep_report=True, diff=False)
 
-        # write low_mem_variant.code to file
-        log.info(f"Write variant {var.value} to {variant_path}")
-        with open(variant_path, "w") as f:
-            f.write(variant.code)
-
-        # format file with black
-        run_black(variant_path, capture_output=True)
-        # check with pyright if it is valid python
-
-    # Minify file with pyminifier
-    log.info(f"\nMinifying to {minified_path.name}")
-    minify(variant_path, minified_path, keep_report=True, diff=False)
-
-    # cross compile for mpy version with mpy-cross
-    if 0:
-        # PATH -> PATH
-        cross_compile(minified_path, mpy_path)
-
-    if 1:
         # str -> path
         # read minified file
         minified_txt = minified_path.read_text()
-        cross_compile(minified_txt, mpy_path, version="1.19.1")
+        cross_compile(minified_txt, mpy_path, version=version)
 
-    if 0:
-        # custom modules (and skip defaults).
-        custom_stubs = ListChangeSet.from_strings(add=["mycoolpackage", "othermodule"], replace=True)
-        custom_variant = CreateStubsCodemod(ctx, modules=custom_stubs).transform_module(base_module)
 
-        print(custom_variant.code)
+# read base createstubs.py
+base_path = Path.cwd() / "src" / "stubber" / "board"
+create_variants(base_path)
+
+
+if 0:
+    # custom modules (and skip defaults).
+    custom_stubs = ListChangeSet.from_strings(add=["mycoolpackage", "othermodule"], replace=True)
+    custom_variant = CreateStubsCodemod(ctx, modules=custom_stubs).transform_module(base_module)
+
+    print(custom_variant.code)
