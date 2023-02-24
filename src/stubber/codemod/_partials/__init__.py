50dc60a7dacb9b799f2547af11f6d71a00e60343
Jos Verlinde <Jos.Verlinde@Microsoft.com>
2023-02-24 23:08:45 +0100

    codemod/partials: Avoid need to hardcode filenames.

--- src/stubber/codemod/_partials/__init__.py
+++ src/stubber/codemod/_partials/__init__.py
@@ -1,7 +1,7 @@
 from collections import deque
 from enum import Enum
 from pathlib import Path
-from typing import Iterator
+from typing import Iterator, cast
 
 # TODO: this way of accessing the partials is not very robust
 PARTIALS_DIR = Path(__file__).parent.absolute()
@@ -18,28 +18,33 @@ def _read_partial(path: Path) -> Iterator[str]:
     lines = deque(path.read_text().splitlines(keepends=True))
     _start = False
     _end = False
+    # todo: allow processing of files that do not have the markers
     while True:
-        if not _start and (line := lines.popleft()):
-            _start = line.strip() == PARTIAL_START
-        if not _end and (line := lines.pop()):
-            _end = line.strip() == PARTIAL_END
-        if _start and _end:
-            break
+        try:
+            if not _start and (line := lines.popleft()):
+                _start = line.strip() == PARTIAL_START
+            if not _end and (line := lines.pop()):
+                _end = line.strip() == PARTIAL_END
+            if _start and _end:
+                break
+        except IndexError:
+            # or avoid erroring out if the file does not have the markers
+            raise ValueError(f"Partial {path} does not have ###PARTIAL### and ###PARTIALEND### markers")
+
     yield from lines
 
 
-class Partial(Enum):
-    DB_MAIN = "db_main"
-    LVGL_MAIN = "lvgl_main"
-
-    @property
-    def partial_path(self) -> Path:
-        # TODO: this way of accessing the partials is not very robust
-        return (PARTIALS_DIR / self.value).with_suffix(".py")
-
+class _PartialMember:
     def contents(self) -> str:
-        """Return the contents of the partial"""	
-        return "".join(_read_partial(self.partial_path))
+        """Return the contents of the partial."""
+        return "".join(_read_partial(cast(Enum, self).value))
+
+
+Partial = Enum(
+    "Partial",
+    [(p.stem.upper(), p.absolute()) for p in PARTIALS_DIR.glob("*.py")],
+    type=_PartialMember,
+)
 
 
 __all__ = ["Partial"]
