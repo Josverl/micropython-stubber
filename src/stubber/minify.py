89e4e63c2e7ce8facc365b4e63e9c1530f89b626
Jos Verlinde <Jos.Verlinde@Microsoft.com>
2023-02-26 00:08:42 +0100

    fix:minify
    Signed-off-by: Jos Verlinde <Jos.Verlinde@Microsoft.com>

--- src/stubber/minify.py
+++ src/stubber/minify.py
@@ -305,10 +305,12 @@ def cross_compile(
         temp_file = Path(temp_file)
         temp_file.write_text(source)
         source_file = temp_file
-    if version:
-        cmd = ["pipx", "run", f"mpy-cross=={version}"]
-    else:
-        cmd = ["mpy-cross"]
+
+    cmd = [
+        "pipx",
+        "run",
+        f"mpy-cross=={version}" if version else "mpy-cross",
+    ]
     # Add params
     cmd += ["-O2", str(source_file), "-o", str(target), "-s", "createstubs.py"]
     log.trace(" ".join(cmd))
