8213a29c33b028e36dd52809e89f95962e85c45a
Jos Verlinde <Jos.Verlinde@Microsoft.com>
2023-02-28 23:56:03 +0100

    re-enable codecov
    Signed-off-by: Jos Verlinde <Jos.Verlinde@Microsoft.com>

--- scripts/coverage-local.ps1
+++ scripts/coverage-local.ps1
@@ -1,10 +1,14 @@
 # Install-Module -Name CredentialManagement
-# Add-StoredCredentials  -UserName "codecov@micropython-stubber" -Password "123" 
+# Add-StoredCredentials  -UserName "codecov@micropython-stubber" -Password "87b31e69-0deb-4932-aed1-6d7ce81ceaa3" 
+
+$ProgressPreference = 'SilentlyContinue'
+Invoke-WebRequest -Uri https://uploader.codecov.io/latest/windows/codecov.exe -Outfile codecov.exe
 
 # run tests and create coverage reports
 coverage run -m pytest tests  --junitxml=results/test-results.xml
 
 coverage lcov
+coverage lcov  -o results/coverage.lcov
 #coverage xml
 # coverage json
 coverage html
@@ -13,6 +17,7 @@ start coverage\index.html
 
 $env:CODECOV_TOKEN = (Get-ClearTextStoredCredentials  -Target "codecov@micropython-stubber").Password
 \develop\tools\codecov.exe -t ${env:CODECOV_TOKEN} -f results/cover*.lcov
+
 # \develop\tools\codecov.exe -t ${env:CODECOV_TOKEN} results/cover*.xml
 # \develop\tools\codecov.exe -t ${env:CODECOV_TOKEN} results/cover*.json
 
