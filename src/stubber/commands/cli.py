ea02b1f3b6b34fbc1fbceba9b132428cf2ac0195
Jos Verlinde <Jos.Verlinde@Microsoft.com>
2023-04-05 10:37:51 +0200

    refactor log function
    Signed-off-by: Jos Verlinde <Jos.Verlinde@Microsoft.com>

--- src/stubber/commands/cli.py
+++ src/stubber/commands/cli.py
@@ -23,20 +23,29 @@ def stubber_cli(ctx: click.Context, verbose: int = 0) -> None:
     # ensure that ctx.obj exists and is a dict (in case `cli()` is called
     ctx.ensure_object(dict)
     # replace std log handler with a custom one capped on INFO level
-    log.remove()
-    level = {0: "INFO", 1: "DEBUG", 2: "TRACE"}.get(verbose, "TRACE")
-    if level == "INFO":
-        format_str = (
-            "<green>{time:YY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{module: <18}</cyan> - <level>{message}</level>"
-        )
-    else:
-        format_str = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
-
-    log.add(sys.stderr, level=level, backtrace=True, diagnose=True, colorize=True, format=format_str)
-    log.info(f"micropython-stubber {__version__}")
+    level = set_loglevel(verbose)
 
     if level != "INFO":
         log.info(f"Log level set to {level}")
     # save info in context for other CLICK modules to use
     ctx.obj["loglevel"] = level
     ctx.obj["verbose"] = verbose
+
+
+def set_loglevel(verbose: int) -> str:
+    """Set log level based on verbose level
+    Get the level from the verbose setting (0=INFO, 1=DEBUG, 2=TRACE)
+    Set the format string, based on the level.
+    Add the handler to the logger, with the level and format string.
+    Return the level
+    """
+    log.remove()
+    level = {0: "INFO", 1: "DEBUG", 2: "TRACE"}.get(verbose, "TRACE")
+    if level == "INFO":
+        format_str = "<green>{time:HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{module: <18}</cyan> - <level>{message}</level>"
+    else:
+        format_str = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
+
+    log.add(sys.stderr, level=level, backtrace=True, diagnose=True, colorize=True, format=format_str)
+    log.info(f"micropython-stubber {__version__}")
+    return level
