a151dffa8b3e6f2f1f2886eb90a3fb6812bea593
Jos Verlinde <Jos_Verlinde@hotmail.com>
2023-04-05 10:45:34 +0200

    add board_info
    Signed-off-by: Jos Verlinde <Jos_Verlinde@hotmail.com>

--- scripts/get-boardnames.py
+++ scripts/get-boardnames.py
@@ -2,18 +2,22 @@ from dataclasses import dataclass, is_dataclass, asdict
 
 from pathlib import Path
 import re
+from typing import List
 from tabulate import tabulate
 import json
+
 
 @dataclass
 class Board:
     """MicroPython Board definition"""
+
     port: str
     board: str
     board_name: str
     mcu_name: str
     description: str
     path: Path
+
 
 # look for all mpconfigboard.h files and extract the board name
 # from the #define MICROPY_HW_BOARD_NAME "PYBD_SF6"
@@ -24,7 +28,7 @@ re_board_name = re.compile(r"#define MICROPY_HW_BOARD_NAME\s+\"(.+)\"")
 re_mcu_name = re.compile(r"#define MICROPY_HW_MCU_NAME\s+\"(.+)\"")
 
 
-l=[]
+board_list: List[Board] = []
 
 for path in mpy_path.glob("**/mpconfigboard.h"):
     board = path.parent.name
@@ -42,21 +46,28 @@ for path in mpy_path.glob("**/mpconfigboard.h"):
             if found == 2:
                 break
     description = f"{board_name} with {mcu_name}" if mcu_name != "-" else board_name
-    l.append(Board(port, board, board_name, mcu_name, description, path))
+    board_list.append(Board(port, board, board_name, mcu_name, description, path))
 
-print(f"Found {len(l)} board definitions.")
+print(f"Found {len(board_list)} board definitions.")
 
-print(tabulate(l, headers="keys"))
+print(tabulate(board_list, headers="keys"))
 
 
 class EnhancedJSONEncoder(json.JSONEncoder):
-        def default(self, o):
-            if is_dataclass(o):
-                return asdict(o)
-            elif isinstance(o, Path):
-                return o.as_posix()
-            return super().default(o)
+    def default(self, o):
+        if is_dataclass(o):
+            return asdict(o)
+        elif isinstance(o, Path):
+            return o.as_posix()
+        return super().default(o)
+
 
 # write the list to json file
 with open("src/stubber/data/board_info.json", "w") as f:
-    json.dump(l, f, indent=4, cls=EnhancedJSONEncoder)
+    json.dump(board_list, f, indent=4, cls=EnhancedJSONEncoder)
+
+# create a csv with only the board and the description of the board_list
+with open("src/stubber/data/board_info.csv", "w") as f:
+    f.write("board,description\n")
+    for board in board_list:
+        f.write(f"{board.description},{board.board}\n")
