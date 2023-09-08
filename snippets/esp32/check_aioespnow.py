2fceb8de78d9bd95a69852c425e4c802fa695b3b
Jos Verlinde <jos_verlinde@hotmail.com>
2023-09-08 19:18:27 +0200

    update snippets and check
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

--- snippets/esp32/check_aioespnow.py
+++ snippets/esp32/check_aioespnow.py
@@ -1,27 +1,31 @@
-# Async 
-# TODO : how is aioespnow documented / detected 
-# Todo : add inheritance to table 
+# typed: ignore
+# Async
+# TODO : how is aioespnow documented / detected
+# Todo : add inheritance to table
 
-import network
-import aioespnow 
 import asyncio
+
+import aioespnow  # type: ignore
+import network
 
 # A WLAN interface must be active to send()/recv()
 network.WLAN(network.STA_IF).active(True)
 
 e = aioespnow.AIOESPNow()  # Returns AIOESPNow enhanced with async support
 e.active(True)
-peer = b'\xbb\xbb\xbb\xbb\xbb\xbb'
+peer = b"\xbb\xbb\xbb\xbb\xbb\xbb"
 e.add_peer(peer)
+
 
 # Send a periodic ping to a peer
 async def heartbeat(e, peer, period=30):
     while True:
-        if not await e.asend(peer, b'ping'):
+        if not await e.asend(peer, b"ping"):
             print("Heartbeat: peer not responding:", peer)
         else:
             print("Heartbeat: ping", peer)
         await asyncio.sleep(period)
+
 
 # Echo any received messages back to the sender
 async def echo_server(e):
@@ -30,13 +34,15 @@ async def echo_server(e):
         try:
             await e.asend(mac, msg)
         except OSError as err:
-            if len(err.args) > 1 and err.args[1] == 'ESP_ERR_ESPNOW_NOT_FOUND':
+            if len(err.args) > 1 and err.args[1] == "ESP_ERR_ESPNOW_NOT_FOUND":
                 e.add_peer(mac)
                 await e.asend(mac, msg)
+
 
 async def main(e, peer, timeout, period):
     asyncio.create_task(heartbeat(e, peer, period))
     asyncio.create_task(echo_server(e))
     await asyncio.sleep(timeout)
+
 
 asyncio.run(main(e, peer, 120, 10))
