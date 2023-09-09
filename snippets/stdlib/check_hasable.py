7d6585ce18a3d9ce91004495caa2d1ac015f43ee
Jos Verlinde <jos_verlinde@hotmail.com>
2023-09-09 18:48:09 +0200

    update snippets
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

--- snippets/stdlib/check_hasable.py
+++ snippets/stdlib/check_hasable.py
@@ -4,3 +4,7 @@ ref: https://github.com/Josverl/micropython-stubs/issues/723
 
 i = 0
 d = {i: "a"}
+
+type_text = "int"
+if type_text in {"int", "float", "str", "bool", "tuple", "list", "dict"}:
+    order = 1
