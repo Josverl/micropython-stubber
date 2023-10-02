7fc3e69f606a513d710cd8a1c9d49d4418b632db
Jos Verlinde <jos_verlinde@hotmail.com>
2023-10-02 22:58:31 +0200

    Test: Fix pytest collect in root
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

--- tests/createstubs/conftest.py
+++ tests/createstubs/conftest.py
@@ -4,17 +4,5 @@ from importlib import import_module
 
 UName = namedtuple("UName", ["sysname", "nodename", "release", "version", "machine"])
 
-LOCATIONS = ["board", pytest.param("minified", marks=pytest.mark.minified)]
-VARIANTS = ["createstubs", "createstubs_mem", "createstubs_db"]
 
 
-def import_variant(location: str, variant: str, minified: bool = False):
-    # sourcery skip: assign-if-exp
-    """Import the variant module and return it."""
-    if location == "minified":
-        minified = True
-    if minified:
-        mod_name = f".board.{variant}_min"
-    else:
-        mod_name = f".board.{variant}"
-    return import_module(mod_name, "stubber")  # type: ignore
