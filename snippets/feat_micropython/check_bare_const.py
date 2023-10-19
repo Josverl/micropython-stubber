efb7858501ad873f051790d426013b999cf1953d
Jos Verlinde <jos_verlinde@hotmail.com>
2023-10-20 01:51:09 +0200

    Revert bare const test.
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

--- snippets/feat_micropython/check_bare_const.py
+++ snippets/feat_micropython/check_bare_const.py
@@ -2,4 +2,5 @@
 # # ref https://github.dev/microsoft/pyright/blob/3cc4e6ccdde06315f5682d9cf61c51ce6fac2753/docs/builtins.md#L7
 # OK:  pyright 1.1.218 can handle this
 
-FOO = const(11) 
+FOO = const(11)  # type: ignore
+# false test outcome : https://github.com/Josverl/micropython-stubber/issues/429
