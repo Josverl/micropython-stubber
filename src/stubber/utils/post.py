b57a900fc7d87ceb5a53bebb308f50e538abf1f8
Jos Verlinde <jos_verlinde@hotmail.com>
2023-11-12 15:55:51 +0000

    feat: Use autoflake as a library i.s.o. cmdline
    Signed-off-by: GitHub <noreply@github.com>

--- src/stubber/utils/post.py
+++ src/stubber/utils/post.py
@@ -3,6 +3,7 @@ import subprocess
 from pathlib import Path
 from typing import List
 
+import autoflake
 from loguru import logger as log
 
 from .stubmaker import generate_pyi_files
@@ -42,44 +43,83 @@ def run_autoflake(path: Path, capture_output: bool = False, process_pyi: bool = 
     needs to be run BEFORE black otherwise it does not recognize long import from`s.
     note: is run file-by-file to include processing .pyi files
     """
-    ret = 0
-    autoflake_cmd = [
-        "autoflake",
-        "-r",
-        "--in-place",
-        # "--remove-all-unused-imports",
-        # "--ignore-init-module-imports",
-        path.as_posix(),
-        "-v",
-        "-v",  # show some feedback
-    ]
-    log.debug("Running autoflake on: {}".format(path))
-    # subprocess.run(cmd, capture_output=log.level >= logging.INFO)
-    result = subprocess.run(autoflake_cmd, capture_output=capture_output, shell=False)
-    if result.returncode != 0:  # pragma: no cover
-        # retry with shell=True
-        result = subprocess.run(autoflake_cmd, capture_output=capture_output, shell=True)
-        if result.returncode != 0:  # pragma: no cover
-            log.warning(f"autoflake failed on: {path}")
-            ret = result.returncode
-
+    if not path.exists():
+        log.warning(f"Path does not exist: {path}")
+        return -1
+    log.info(f"Running autoflake on: {path}")
+    # create a list of files to be formatted
+    files: List[str] = []
+    files.extend([str(f) for f in path.rglob("*.py")])
     if process_pyi:
-        for file in list(path.rglob("*.pyi")):
-            autoflake_cmd = [
-                "autoflake",
-                "-r",
-                "--in-place",
-                # "--remove-all-unused-imports",
-                # "--ignore-init-module-imports",
-                file.as_posix(),
-                "-v",
-                "-v",  # show some feedback
-            ]
-            log.trace("Running autoflake on: {}".format(path))
-            # subprocess.run(cmd, capture_output=log.level >= logging.INFO)
-            result = subprocess.run(autoflake_cmd, capture_output=capture_output)
-            if result.returncode != 0:
-                log.warning(f"autoflake failed on: {file}")
-                ret = result.returncode
+        files.extend([str(f) for f in path.rglob("*.pyi")])
 
-    return ret
+    # build an argument list
+    autoflake_args = {
+        "write_to_stdout": False, # print changed text to stdout
+        "in_place": True, # make changes to files instead of printing diffs
+        "remove_all_unused_imports": False,
+        "ignore_init_module_imports": False,  # exclude __init__.py when removing unused imports
+        "expand_star_imports": False,
+        "remove_duplicate_keys": False,
+        "remove_unused_variables": False,  # remove all unused imports (not just those from the standard library)
+        "remove_rhs_for_unused_variables": False,
+        "ignore_pass_statements": True,
+        "ignore_pass_after_docstring": False,  # ignore pass statements after a newline ending on '"""'
+        "check": False,  # return error code if changes are needed
+        "check_diff": False,
+        "quiet": False,
+    }
+    # format the files
+    exit_status = 0
+    for name in files:
+        log.debug(f"Running autoflake on: {name}")
+        exit_status |= autoflake.fix_file(name, args=autoflake_args)
+
+
+# def run_autoflake_old(path: Path, capture_output: bool = False, process_pyi: bool = False):
+#     """
+#     run autoflake to remove unused imports
+#     needs to be run BEFORE black otherwise it does not recognize long import from`s.
+#     note: is run file-by-file to include processing .pyi files
+#     """
+#     ret = 0
+#     autoflake_cmd = [
+#         "autoflake",
+#         "-r",
+#         "--in-place",
+#         # "--remove-all-unused-imports",
+#         # "--ignore-init-module-imports",
+#         path.as_posix(),
+#         "-v",
+#         "-v",  # show some feedback
+#     ]
+#     log.debug("Running autoflake on: {}".format(path))
+#     # subprocess.run(cmd, capture_output=log.level >= logging.INFO)
+#     result = subprocess.run(autoflake_cmd, capture_output=capture_output, shell=False)
+#     if result.returncode != 0:  # pragma: no cover
+#         # retry with shell=True
+#         result = subprocess.run(autoflake_cmd, capture_output=capture_output, shell=True)
+#         if result.returncode != 0:  # pragma: no cover
+#             log.warning(f"autoflake failed on: {path}")
+#             ret = result.returncode
+
+#     if process_pyi:
+#         for file in list(path.rglob("*.pyi")):
+#             autoflake_cmd = [
+#                 "autoflake",
+#                 "-r",
+#                 "--in-place",
+#                 # "--remove-all-unused-imports",
+#                 # "--ignore-init-module-imports",
+#                 file.as_posix(),
+#                 "-v",
+#                 "-v",  # show some feedback
+#             ]
+#             log.trace("Running autoflake on: {}".format(path))
+#             # subprocess.run(cmd, capture_output=log.level >= logging.INFO)
+#             result = subprocess.run(autoflake_cmd, capture_output=capture_output)
+#             if result.returncode != 0:
+#                 log.warning(f"autoflake failed on: {file}")
+#                 ret = result.returncode
+
+#     return ret
