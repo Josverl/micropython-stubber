0842761806017a28f1a03a1f6ff0a6dc40881b9e
Jos Verlinde <jos_verlinde@hotmail.com>
2023-12-18 09:31:21 +0100

    tests: add dup import test
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>
    (cherry picked from commit 02ff5e037c6e6c98fc549336bed37258c35191e0)

---
+++ tests/codemods/codemod_test_cases/import_from_add_existing/stub.py
@@ -0,0 +1,15 @@
+"""
+add from x import y for existing from x import y
+stub
+"""
+from typing import Tuple
+
+from _typeshed import Incomplete
+
+
+def foo() -> Incomplete:
+    ...
+
+
+e: float
+"""base of the natural logarithm"""
