2c33a0e5a749df1c8249ba6984181b883c2c62de
Josverl <Josverl@users.noreply.github.com>
2023-12-25 14:48:02 +0000

    Update mip packages

--- mip/v5/createstubs_db_min.py
+++ mip/v5/createstubs_db_min.py
@@ -42,7 +42,7 @@ try:from machine import reset
 except P:pass
 try:from collections import OrderedDict as d
 except P:from ucollections import OrderedDict as d
-__version__='v1.15.1'
+__version__='v1.15.2a0'
 w=2
 x=2
 e=[J,'/lib','/sd/lib','/flash/lib','lib']
