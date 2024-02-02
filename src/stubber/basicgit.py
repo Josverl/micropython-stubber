97930692d04c4c38e3fb785bddaf1bf042a06093
Jos Verlinde <jos_verlinde@hotmail.com>
2024-02-02 08:17:33 +0100

    chore: update GH token
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

--- src/stubber/basicgit.py
+++ src/stubber/basicgit.py
@@ -13,9 +13,10 @@ from github import Auth, Github
 from loguru import logger as log
 from packaging.version import parse
 
-# Token with no permissions
+# Token with no permissions to avoid throttling
+# https://docs.github.com/en/rest/using-the-rest-api/rate-limits-for-the-rest-api?apiVersion=2022-11-28#getting-a-higher-rate-limit
 PAT_NO_ACCESS = (
-    "github_pat" + "_11AAHPVFQ0IwtAmfc3cD5Z" + "_xOVII22ErRzzZ7xwwxRcNotUu4krMMbjinQcsMxjnWkYFBIDRWFlZMaHSqq"
+    "github_pat" + "_11AAHPVFQ0qAkDnSUaMKSp" + "_ZkDl5NRRwBsUN6EYg9ahp1Dvj4FDDONnXVgimxC2EtpY7Q7BUKBoQ0Jq72X"
 )
 PAT = os.environ.get("GITHUB_TOKEN") or PAT_NO_ACCESS
 GH_CLIENT = Github(auth=Auth.Token(PAT))
