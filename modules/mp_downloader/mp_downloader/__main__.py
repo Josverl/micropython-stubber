9b849b8aed5d8b5f760b7363297fd2f326cccb2f
Jos Verlinde <jos.verlinde@microsoft.com>
2024-02-17 22:21:16 +0100

    add module to download micropython firmwares
    Signed-off-by: Jos Verlinde <jos.verlinde@microsoft.com>

---
+++ modules/mp_downloader/mp_downloader/__main__.py
@@ -0,0 +1,206 @@
+import functools
+import itertools
+import re
+from pathlib import Path
+from typing import Dict, List, Optional
+from urllib.parse import urljoin
+
+import requests
+from bs4 import BeautifulSoup
+from loguru import logger as log
+
+MICROPYTHON_ORG_URL = "https://micropython.org/"
+# ports and firmware extensions we are interested in
+PORT_FWTYPES = {
+    "stm32": ".hex",
+    "esp32": ".bin",
+    "esp8266": ".bin",
+    "rp2": ".uf2",
+    "samd": ".uf2",
+}
+
+
+# use functools.lru_cache to avoid needing to download pages multiple times
+@functools.lru_cache(maxsize=500)
+def get_page(page_url: str) -> str:
+    """Get the HTML of a page and return it as a string."""
+    response = requests.get(page_url)
+    return response.content.decode()
+
+
+@functools.lru_cache(maxsize=500)
+def get_board_urls(page_url: str) -> List[Dict[str, str]]:
+    """
+    Get the urls to all the board pages listed on this page.
+    Assumes that all links to firmware  have "class": "board-card"
+    """
+    downloads_html = get_page(page_url)
+    soup = BeautifulSoup(downloads_html, "html.parser")
+    tags = soup.findAll("a", recursive=True, attrs={"class": "board-card"})
+    # assumes that all links are relative to the page url
+    boards = [tag.get("href") for tag in tags]
+    if "?" in page_url:
+        page_url = page_url.split("?")[0]
+    return [{"board": board, "url": page_url + board} for board in boards]
+
+
+def firmware_list(board_url: str, base_url: str, ext: str) -> List[str]:
+    """Get the urls to all the firmware files for a board."""
+    html = get_page(board_url)
+    soup = BeautifulSoup(html, "html.parser")
+    # get all the a tags:
+    #  1. that have a url that starts with `/resources/firmware/`
+    #  2. end with a matching extension for this port.
+    tags = soup.findAll(
+        "a",
+        recursive=True,
+        attrs={"href": re.compile(r"^/resources/firmware/.*\." + ext.lstrip(".") + "$")},
+    )
+    if "?" in base_url:
+        base_url = base_url.split("?")[0]
+    links: List = [urljoin(base_url, tag.get("href")) for tag in tags]
+    return links
+
+
+# Regexes to remove datses and hashes in the filename that just get in the way
+RE_DATE = r"(-\d{8}-)"
+RE_HASH = r"(.g[0-9a-f]+\.)"
+# regex to extract the version from the firmware filename
+RE_VERSION_PREVIEW = r"(\d+\.\d+(\.\d+)?(-\w+.\d+)?)"
+
+FirmwareInfo = Dict[str, str]
+
+
+# boards we are interested in ( this avoids getting a lot of boards we don't care about)
+# The first run takes ~60 seconds to run for 4 ports , all boards
+# so it makes sense to cache the results and skip boards as soon as possible
+def get_boards(fw_types, board_list: List[str]) -> List[FirmwareInfo]:
+    board_urls: List[FirmwareInfo] = []
+    for port in fw_types.keys():
+        download_page_url = f"{MICROPYTHON_ORG_URL}download/?port={port}"
+        _urls = get_board_urls(download_page_url)
+        # filter out boards we don't care about
+        _urls = [board for board in _urls if board["board"] in board_list]
+        # add the port to the board urls
+        for board in _urls:
+            board["port"] = port
+
+        for board in _urls:
+            # add a board to the list for each firmware found
+            firmwares = firmware_list(board["url"], MICROPYTHON_ORG_URL, fw_types[port])
+            for _url in firmwares:
+                board["firmware"] = _url
+                board["preview"] = "preview" in _url  # type: ignore
+                if ver_match := re.search(RE_VERSION_PREVIEW, _url):
+                    board["version"] = ver_match[1]
+                else:
+                    board["version"] = ""
+                if "preview." in board["version"]:
+                    board["build"] = board["version"].split("preview.")[-1]
+                else:
+                    board["build"] = "0"
+                # remove date from firmware name
+                fname = re.sub(RE_DATE, "-", Path(board["firmware"]).name)
+                # remove hash from firmware name
+                fname = re.sub(RE_HASH, ".", fname)
+                board["filename"] = fname
+                board["variant"] = board["filename"].split("-v")[0] if "-v" in board["filename"] else ""
+                board_urls.append(board.copy())
+    return board_urls
+
+
+def key_fw_variant_ver(x: FirmwareInfo):
+    "sorting key for the retrieved board urls"
+    return x["variant"], x["version"], x["preview"], x["build"]
+
+
+def key_fw_variant(x: FirmwareInfo):
+    "Grouping key for the retrieved board urls"
+    return x["variant"], x["preview"]
+
+
+def download_firmwares(
+    firmware_folder: Path,
+    board_list: List[str],
+    version_list: Optional[List[str]] = None,
+    *,
+    preview: bool = False,
+    force: bool = False,
+):
+    if version_list is None:
+        version_list = []
+    unique_boards = get_firmware_list(board_list, version_list, preview)
+
+    for b in unique_boards:
+        print(b["filename"])
+    # relevant
+
+    print(f"Found {len(unique_boards)} relevant unique firmwares")
+
+    firmware_folder.mkdir(exist_ok=True)
+
+    import json
+
+    with open(firmware_folder / "firmware.jsonl", "a", encoding="utf-8", buffering=1) as f_jsonl:
+        for board in unique_boards:
+            filename = firmware_folder / board["port"] / board["filename"]
+            filename.parent.mkdir(exist_ok=True)
+            if filename.exists() and not force:
+                log.info(f" {filename} already exists, skip download")
+                continue
+            print(f"Downloading {board['firmware']} to {filename}")
+            try:
+                r = requests.get(board["firmware"], allow_redirects=True)
+                with open(filename, "wb") as fw:
+                    fw.write(r.content)
+                board["filename"] = str(filename)
+            except requests.RequestException as e:
+                log.exception(e)
+                continue
+            json_str = json.dumps(board) + "\n"
+            f_jsonl.write(json_str)
+
+
+def get_firmware_list(board_list: List[str], version_list: List[str], preview: bool):
+    log.info("Checking MicroPython download pages")
+
+    board_urls = sorted(get_boards(PORT_FWTYPES, board_list), key=key_fw_variant_ver)
+
+    log.info(f"Total {len(board_urls)} firmwares")
+    relevant = [
+        board
+        for board in board_urls
+        if board["board"] in board_list and (board["version"] in version_list or board["preview"] and preview)
+        # and b["port"] in ["esp32", "rp2"]
+    ]
+    log.debug(f"Matching firmwares: {len(relevant)}")
+    # select the unique boards
+    unique_boards: List[FirmwareInfo] = []
+    for _, g in itertools.groupby(relevant, key=key_fw_variant):
+        # list is aleady sorted by build so we can just get the last item
+        sub_list = list(g)
+        unique_boards.append(sub_list[-1])
+    log.info(f"Last preview only: {len(unique_boards)}")
+    return unique_boards
+
+
+RELEVANT_BOARDS = [
+    "PYBV11",
+    "ESP32_GENERIC",
+    "ESP32_GENERIC_S3",
+    "RPI_PICO",
+    "RPI_PICO_W",
+    "ARDUINO_NANO_RP2040_CONNECT",
+    "PIMORONI_PICOLIPO_16MB",
+    "SEEED_WIO_TERMINAL",
+]
+
+
+def cli():
+    firmware_folder = Path("firmware")
+    versions = []
+    download_firmwares(firmware_folder, RELEVANT_BOARDS, versions, preview=True)
+
+
+if __name__ == "__main__":
+    exit(cli())
