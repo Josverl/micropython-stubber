5b3d72b485b1cab4574c9b81bf5ff39611ae7d7b
Jos Verlinde <jos.verlinde@microsoft.com>
2024-03-16 16:33:10 +0100

    Update testpaths and norecursedirs in pyproject.toml*** ***Update coverage-local.ps1 script*** ***Refactor test_sort.py in src/mpflash/tests

--- scripts/coverage-local.ps1
+++ scripts/coverage-local.ps1
@@ -5,18 +5,18 @@ $ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri https://uploader.codecov.io/latest/windows/codecov.exe -Outfile codecov.exe
 
 # run tests and create coverage reports
-coverage run -m pytest tests  --junitxml=results/test-results.xml
+# coverage run -m pytest -k mpflash  --junitxml=results/test-results.xml
+coverage run -m pytest  --junitxml=results/test-results.xml
 
 coverage lcov
 coverage lcov  -o results/coverage.lcov
 #coverage xml
 # coverage json
-coverage html
-
+coverage html 
 start coverage\index.html
 
 $env:CODECOV_TOKEN = (Get-ClearTextStoredCredentials  -Target "codecov@micropython-stubber").Password
-\develop\tools\codecov.exe -t ${env:CODECOV_TOKEN} -f results/cover*.lcov
+.\codecov.exe -t ${env:CODECOV_TOKEN} -f results/cover*.lcov
 
 # \develop\tools\codecov.exe -t ${env:CODECOV_TOKEN} results/cover*.xml
 # \develop\tools\codecov.exe -t ${env:CODECOV_TOKEN} results/cover*.json
--- src/mpflash/tests/test_sort.py
+++ src/mpflash/tests/test_sort.py
@@ -1,18 +1,21 @@
-# import pytest
-# from pathlib import Path
-# import mpflash.flasher as flasher
-# from mpflash.config import config
+from pathlib import Path
+
+import pytest
+
+from mpflash.config import config
+from mpflash.flash import find_firmware, local_firmwares
 
 
-# def test_load(tmp_path: Path):
-#     fw_folder = Path("/home/jos/projects/micropython-stubber/firmware")
-#     all = flasher.load_firmwares(config.firmware_folder)
-#     assert len(all) > 0
+@pytest.mark.mpflash
+def test_load(tmp_path: Path):
+    # fw_folder = Path("/home/jos/projects/micropython-stubber/firmware")
+    all = local_firmwares(config.firmware_folder)
+    assert len(all) > 0
 
 
-# def test_find():
-#     fw_folder = Path("/home/jos/projects/micropython-stubber/firmware")
-#     fws = flasher.find_firmware(fw_folder=fw_folder, port="samd", board="SEEED_WIO_TERMINAL",preview=True)
-#     assert len(fws) > 0
-#     assert fws[0]["board"] == "SEEED_WIO_TERMINAL"
-#     assert int(fws[-1]["build"]) == 155
+def test_find():
+    fw_folder = Path(config.firmware_folder)
+    fws = find_firmware(fw_folder=fw_folder, port="samd", board="SEEED_WIO_TERMINAL", preview=True)
+    assert len(fws) > 0
+    assert fws[0]["board"] == "SEEED_WIO_TERMINAL"
+    assert int(fws[-1]["build"]) > 0
