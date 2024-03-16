ce2f39d76ea7830f12d9256e16b26276bab5353b
Jos Verlinde <jos.verlinde@microsoft.com>
2024-03-16 18:48:06 +0100

    Add testdata for mpflash

--- src/mpflash/tests/test_sort.py
+++ src/mpflash/tests/test_sort.py
@@ -8,14 +8,16 @@ from mpflash.flash import find_firmware, local_firmwares
 
 @pytest.mark.mpflash
 def test_load(tmp_path: Path):
-    # fw_folder = Path("/home/jos/projects/micropython-stubber/firmware")
-    all = local_firmwares(config.firmware_folder)
+    test_fw_folder = Path(__file__).parent / "data" / "firmware"
+    assert test_fw_folder.exists()
+    all = local_firmwares(test_fw_folder)
     assert len(all) > 0
 
 
 def test_find():
-    fw_folder = Path(config.firmware_folder)
-    fws = find_firmware(fw_folder=fw_folder, port="samd", board="SEEED_WIO_TERMINAL", preview=True)
+    test_fw_folder = Path(__file__).parent / "data" / "firmware"
+    assert test_fw_folder.exists()
+    fws = find_firmware(fw_folder=test_fw_folder, port="samd", board="SEEED_WIO_TERMINAL", preview=True)
     assert len(fws) > 0
     assert fws[0]["board"] == "SEEED_WIO_TERMINAL"
     assert int(fws[-1]["build"]) > 0
