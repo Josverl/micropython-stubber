e06092d4dfb1dd6e6f139cac2415f466e5cffeec
Jos Verlinde <jos.verlinde@microsoft.com>
2024-03-17 17:41:33 +0100

    Refactor test coverage and add mpflash and stubber coverage reports

--- scripts/coverage-local.ps1
+++ scripts/coverage-local.ps1
@@ -1,12 +1,12 @@
 # Install-Module -Name CredentialManagement
-# Add-StoredCredentials  -UserName "codecov@micropython-stubber" -Password "87b31e69-0deb-4932-aed1-6d7ce81ceaa3" 
+# Add-StoredCredentials  -UserName "codecov@micropython-stubber" -Password "e31c6aa7-0888-4666-aa9e-d486249b5c1e" 
 
 $ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri https://uploader.codecov.io/latest/windows/codecov.exe -Outfile codecov.exe
 
 # run tests and create coverage reports
-# coverage run -m pytest -k mpflash  --junitxml=results/test-results.xml
-coverage run -m pytest  --junitxml=results/test-results.xml
+&coverage run -m pytest -m mpflash --junitxml=results/cov-mpflash.xml
+&coverage run -m pytest -m stubber --junitxml=results/cov-stubber.xml
 
 coverage lcov
 coverage lcov  -o results/coverage.lcov
@@ -16,7 +16,8 @@ coverage html
 start coverage\index.html
 
 $env:CODECOV_TOKEN = (Get-ClearTextStoredCredentials  -Target "codecov@micropython-stubber").Password
-.\codecov.exe -t ${env:CODECOV_TOKEN} -f results/cover*.lcov
+.\codecov.exe -t ${env:CODECOV_TOKEN} -f results/cov-mpflash.xml -F mpflash
+.\codecov.exe -t ${env:CODECOV_TOKEN} -f results/cov-stubber.xml -F stubber
 
 # \develop\tools\codecov.exe -t ${env:CODECOV_TOKEN} results/cover*.xml
 # \develop\tools\codecov.exe -t ${env:CODECOV_TOKEN} results/cover*.json
