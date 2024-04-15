4caad648cf081a930a31c55d6c7817bfb7823de6
Jos Verlinde <jos.verlinde@microsoft.com>
2024-04-15 23:08:03 +0200

    stubber: bump version

--- src/stubber/board/createstubs.py
+++ src/stubber/board/createstubs.py
@@ -24,7 +24,7 @@ try:
 except ImportError:
     from ucollections import OrderedDict  # type: ignore
 
-__version__ = "v1.17.6"
+__version__ = "v1.18.0"
 ENOENT = 2
 _MAX_CLASS_LEVEL = 2  # Max class nesting
 LIBS = ["lib", "/lib", "/sd/lib", "/flash/lib", "."]
