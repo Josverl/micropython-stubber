f8e1abd8470f23c45aaa84f07ee73f6f7ed1e61a
Jos Verlinde <jos_verlinde@hotmail.com>
2024-05-06 23:21:59 +0200

    mpflash: Refactor MPRemoteBoard's get_comports method
    The code changes in this commit refactor the `get_comports` method in the `MPRemoteBoard` class. The method now handles the sorting of comports differently based on the operating system. On Windows, the comports are sorted by number, while on other platforms, they are sorted by device name.

--- src/mpflash/mpflash/mpremoteboard/__init__.py
+++ src/mpflash/mpflash/mpremoteboard/__init__.py
@@ -82,8 +82,18 @@ class MPRemoteBoard:
             comports = [p for p in comports if "bluetooth" not in p.description.lower()]
             comports = [p for p in comports if "BTHENUM" not in p.hwid]
         if description:
-            return sorted([f"{p.device} {p.description}" for p in comports])
-        return sorted([p.device for p in comports])
+            output = [
+                f"{p.device} {(p.manufacturer + ' ') if p.manufacturer and not p.description.startswith(p.manufacturer) else ''}{p.description}"
+                for p in comports
+            ]
+        else:
+            output = [p.device for p in comports]
+
+        if sys.platform == "win32":
+            # Windows sort of comports by number - but fallback to device name
+            return sorted(output, key=lambda x: int(x.split()[0][3:]) if x.split()[0][3:].isdigit() else x)
+        # sort by device name
+        return sorted(output)
 
     @retry(stop=stop_after_attempt(RETRIES), wait=wait_fixed(1), reraise=True)  # type: ignore ## retry_error_cls=ConnectionError,
     def get_mcu_info(self, timeout: int = 2):
