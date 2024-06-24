6c2e44a88ed1315c6b782578ead071457e71fd25
Jos Verlinde <jos_verlinde@hotmail.com>
2024-06-24 23:02:06 +0200

    mpflash: Update manifestfile.py to version v1.23.0
    closes #512
    
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

--- tests/freeze/freezer_mpy_test.py
+++ tests/freeze/freezer_mpy_test.py
@@ -58,7 +58,9 @@ def test_get_portboard(path: str, port: str, board: str):
     assert _port == port
 
 
-def test_manifest_uasync(tmp_path: Path, testrepo_micropython: Path, testrepo_micropython_lib: Path):
+def test_manifest_uasync(
+    tmp_path: Path, testrepo_micropython: Path, testrepo_micropython_lib: Path
+):
     "test if task.py is included with the uasyncio frozen module"
     mpy_version = "v1.18"
     mpy_folder = testrepo_micropython.absolute()
@@ -70,7 +72,9 @@ def test_manifest_uasync(tmp_path: Path, testrepo_micropython: Path, testrepo_mi
     manifest = mpy_folder / "ports/esp32/boards/manifest.py"
     freeze_one_manifest_2(manifest, stub_folder, mpy_folder, lib_folder, mpy_version)
 
-    assert (tmp_path / "esp32" / GENERIC_U / "uasyncio/task.py").exists(), "task.py must be included in uasyncio"
+    assert (
+        tmp_path / "esp32" / GENERIC_U / "uasyncio/task.py"
+    ).exists(), "task.py must be included in uasyncio"
 
 
 #######################################################################################################################
@@ -195,7 +199,7 @@ def test_freeze_one_manifest_v2(
         # "v1.19",
         # "v1.19.1",
         "v1.20.0",
-        # "v1.21.0",
+        "v1.23.0",
         "latest",
         "preview",
     ],
@@ -253,7 +257,9 @@ def test_freeze_any_mocked(
 ):
     "mocked test if we can freeze source using manifest.py files"
 
-    m_freeze_folders = mocker.patch("stubber.freeze.get_frozen.freeze_folders", autospec=True, return_value=[1])
+    m_freeze_folders = mocker.patch(
+        "stubber.freeze.get_frozen.freeze_folders", autospec=True, return_value=[1]
+    )
     # m_freeze_one_manifest_1= mocker.patch("stubber.freeze.get_frozen.freeze_one_manifest_1", autospec=True, return_value=1)
     m_freeze_one_manifest_2 = mocker.patch(
         "stubber.freeze.get_frozen.freeze_one_manifest_2", autospec=True, return_value=1
@@ -265,7 +271,7 @@ def test_freeze_any_mocked(
         mpy_lib_path=testrepo_micropython_lib,
     )
     # calls = m_freeze_folders.call_count + m_freeze_one_manifest_1.call_count + m_freeze_one_manifest_2.call_count
-    calls = m_freeze_folders.call_count + m_freeze_one_manifest_2.call_count # type: ignore
+    calls = m_freeze_folders.call_count + m_freeze_one_manifest_2.call_count  # type: ignore
     print(f" m_freeze_folders.call_count {m_freeze_folders.call_count}")
     # print(f" m_freeze_one_manifest_1.call_count {m_freeze_one_manifest_1.call_count}")
     print(f" m_freeze_one_manifest_2.call_count {m_freeze_one_manifest_2.call_count}")
@@ -284,7 +290,9 @@ def test_freeze_manifest2_error_mocked(
 ):
     "mocked test if we can freeze source using manifest.py files"
 
-    m_freeze_folders = mocker.patch("stubber.freeze.get_frozen.freeze_folders", autospec=True, return_value=[1])
+    m_freeze_folders = mocker.patch(
+        "stubber.freeze.get_frozen.freeze_folders", autospec=True, return_value=[1]
+    )
     m_freeze_one_manifest_2 = mocker.patch(
         "stubber.freeze.get_frozen.freeze_one_manifest_2", autospec=True, return_value=1
     )
