0dbd8a27338b7283d43e546a2bac2ace9700f8bc
Jos Verlinde <Jos.Verlinde@microsoft.com>
2024-06-25 22:23:54 +0200

    mpflash: Refactor enter_bootloader_cdc_1200bps method to work on windows
    Fixes: https://github.com/Josverl/micropython-stubber/issues/571
    
    Signed-off-by: Jos Verlinde <Jos.Verlinde@microsoft.com>

--- src/mpflash/mpflash/bootloader/touch1200.py
+++ src/mpflash/mpflash/bootloader/touch1200.py
@@ -7,32 +7,20 @@ from mpflash.errors import MPFlashError
 from mpflash.logger import log
 from mpflash.mpremoteboard import MPRemoteBoard
 
-from .manual import enter_bootloader_manual
 
 
 def enter_bootloader_cdc_1200bps(mcu: MPRemoteBoard, timeout: int = 10):
-    if sys.platform == "win32":
-        log.warning("Touch 1200bps method is currently not supported on Windows, switching to manual")
-        return enter_bootloader_manual(mcu, timeout=timeout)
-
-    log.info(f"Entering bootloader on {mcu.board} on {mcu.serialport} using CDC 1200bps")
-    # if port argument is present perform soft reset
     if not mcu.serialport:
         raise MPFlashError("No serial port specified")
+    log.info(f"Entering bootloader on {mcu.serialport} using CDC toch 1200 bps method")
+    # if port argument is present perform soft reset
     # try to initiate serial port connection on PORT with 1200 baudrate
     try:
-        with serial.Serial(
-            port=mcu.serialport,
-            baudrate=1200,
-            parity=serial.PARITY_NONE,
-            stopbits=serial.STOPBITS_ONE,
-            bytesize=serial.EIGHTBITS,
-            rtscts=True,
-        ) as connection:
-            print("Connection established")
-            connection.rts = True
-            connection.dtr = False
-            time.sleep(0.4)
+        com = serial.Serial(mcu.serialport, 1200, dsrdtr=True)
+        com.rts = False  # required
+        com.dtr = False  # might as well
+        time.sleep(0.2)
+        com.close()
 
     except serial.SerialException as e:
         log.exception(e)
