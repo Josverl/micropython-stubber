48e966c42cc51f969e838a8859cae75921d53971
Jos Verlinde <Jos.Verlinde@microsoft.com>
2024-09-25 22:32:19 +0200

    stubber:Add --params-only to Merge command.
    Signed-off-by: Jos Verlinde <Jos.Verlinde@microsoft.com>

---
+++ tests/codemods/params_test_cases/func_docstring_no_overwrite/expected.py
@@ -0,0 +1,15 @@
+"""
+do not overwrite docstring
+"""
+
+
+def foo(self, iterable) -> None:
+    """
+    original docstring
+    Append new elements as contained in *iterable* to the end of
+    array, growing it.
+    """
+    ...
+
+
+def bar(): ...
---
+++ tests/codemods/params_test_cases/func_param_update_overwrite/doc_stub.py
@@ -0,0 +1,12 @@
+"""
+simple functions 
+"""
+
+from typing import Any
+
+
+def extend(self, iterable: Sequence[Any], /) -> None:
+    """
+    Append new elements as contained in *iterable* to the end of
+    array, growing it.
+    """
