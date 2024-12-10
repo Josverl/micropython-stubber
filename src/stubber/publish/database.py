7c08d98bc86f2885f287bc678be09a22771d74e1
Jos Verlinde <Jos.Verlinde@microsoft.com>
2024-12-10 14:48:38 +0100

    refactor: migrate database access from PysonDB to SQLite
    Signed-off-by: Jos Verlinde <Jos.Verlinde@microsoft.com>

--- src/stubber/publish/database.py
+++ src/stubber/publish/database.py
@@ -1,18 +1,26 @@
 """basic interface to the json database"""
 
+import sqlite3
 from pathlib import Path
 from typing import Union
 
 from pysondb import PysonDB
 
 
-def get_database(publish_path: Union[Path, str], production: bool = False) -> PysonDB:
+def get_database(publish_path: Path, production: bool = False) -> sqlite3.Connection:
     """
-    Open the json database at the given path.
+    Open the sqlite database at the given path.
 
-    The database should be located in a subfolder `/publish` of the root path.
-    The database name is determined by the production flag as `package_data[_test].jsondb`
+    The database should be located in a subfolder `/data` of the root path.
+    The database name is determined by the production flag as `all_packages[_test].db`
     """
+
     publish_path = Path(publish_path)
-    db_path = publish_path / f"package_data{'' if production else '_test'}.jsondb"
-    return PysonDB(db_path.as_posix())
+    db_path = publish_path / f"all_packages{'' if production else '_test'}.db"
+    if not db_path.exists():
+        raise FileNotFoundError("Database file not found")
+
+    conn = sqlite3.connect(db_path)
+    return conn
+
+
