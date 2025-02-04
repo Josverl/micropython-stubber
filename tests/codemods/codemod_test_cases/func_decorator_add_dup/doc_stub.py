6ec4f4433a983f4f3c761e08c5d12a1b0635bf16
Jos Verlinde <Jos.Verlinde@microsoft.com>
2025-02-04 22:30:47 +0100

    stubber: add test cases for function decorator duplication handling
    Signed-off-by: Jos Verlinde <Jos.Verlinde@microsoft.com>

---
+++ tests/codemods/codemod_test_cases/func_decorator_add_dup/doc_stub.py
@@ -0,0 +1,8 @@
+"""
+simple functions 
+"""
+
+
+@lru_cache
+def foo(pin: int, /, limit: int = 100) -> str:
+    ...
---
+++ tests/codemods/codemod_test_cases/func_decorator_add_dup2/expected.py
@@ -0,0 +1,8 @@
+# fmt: off
+"""
+simple functions 
+"""
+@foo
+@lru_cache
+def foo(pin: int, /, limit: int = 100) -> str:
+    ...
