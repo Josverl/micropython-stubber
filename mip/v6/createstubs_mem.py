3aef5113d5931340ebc5c461c84b6b0b8ee1ae35
Josverl <Josverl@users.noreply.github.com>
2025-02-25 20:19:47 +0000

    Update mip packages

--- mip/v6/createstubs_mem.py
+++ mip/v6/createstubs_mem.py
@@ -9,7 +9,7 @@
     - cross compilation, using mpy-cross, 
       to avoid the compilation step on the micropython device 
 
-This variant was generated from createstubs.py by micropython-stubber v1.24.0
+This variant was generated from createstubs.py by micropython-stubber v1.24.2
 """
 
 # Copyright (c) 2019-2024 Jos Verlinde
