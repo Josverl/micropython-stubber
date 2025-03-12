49db5334bb47f9cebb438e55b1df86593a28d5ab
Jos Verlinde <jos_verlinde@hotmail.com>
2025-03-12 23:49:29 +0100

    feat: Add test cases for property decorator methods
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

---
+++ tests/codemods/codemod_test_cases/property_decorator_add/before.py
@@ -0,0 +1,14 @@
+# fmt: off
+"""
+Add a decorator to a property method.
+"""
+
+class Style:
+    """A dict-like interface to an element's `style` attribute."""
+
+    def __init__(self, element: Element) -> None: ...
+
+    @property
+    def visible(self) -> Incomplete: ...
+    @visible.setter
+    def visible(self, value) -> None: ...
