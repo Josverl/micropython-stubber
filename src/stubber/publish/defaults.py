3466fcdef26fc9c5ffacf066b374fd3392b8a3e8
Jos Verlinde <Jos_Verlinde@hotmail.com>
2025-12-02 12:30:44 +0100

    feat: add default board for mimxrt to DEFAULT_BOARDS
    Signed-off-by: Jos Verlinde <Jos_Verlinde@hotmail.com>

--- src/stubber/publish/defaults.py
+++ src/stubber/publish/defaults.py
@@ -15,6 +15,7 @@ DEFAULT_BOARDS: Dict[str, List[str]] = {
     "esp8266": ["ESP8266_GENERIC", "GENERIC", ""],
     "rp2": ["RPI_PICO", "PICO", ""],
     "samd": ["SEEED_WIO_TERMINAL", ""],
+    "mimxrt": ["SEEED_ARCH_MIX", ""],
     "unix": ["standard", ""],
     "windows": ["standard", ""],
     "webassembly": ["pyscript", "standard", ""],
