fee2b6cdb1b7463dadb6911d0139c23ce1e00dec
Jos Verlinde <Jos_Verlinde@hotmail.com>
2026-07-24 03:09:36 +0200

    chore: Pemove old unused pwsh scripts.
    Signed-off-by: Jos Verlinde <Jos_Verlinde@hotmail.com>

--- scripts/make-variants.ps1
+++
@@ -1,8 +0,0 @@
-# create all variants
-
-&stubber make-variants
-&stubber make-variants --target ./mip/v6
-&stubber make-variants --target ./mip/v5 --version v1.18
-
-# todo: update version in 
-# package.json
